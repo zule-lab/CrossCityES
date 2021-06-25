f1d31b4f4c40947c2930e6a9cd5a97c3b756d54e
icrichmond <icrichmond@mun.ca>
2021-06-25 11:06:29 -0400

    Cleaning up script for downloads

--- scripts/1-Download script.R
+++ scripts/1-Download script.R
@@ -17,8 +17,9 @@ cal_tree_raw<-read.csv(cal_dest)
 View(cal_tree_raw)
 
 ## Halifax tree data download
-
-
+hal_URL <- "https://catalogue-hrm.opendata.arcgis.com/datasets/33a4e9b6c7e9439abcd2b20ac50c5a4d_0/explore?location=44.648814%2C-63.600208%2C13.44"
+hal_dest <- "input/hal_tree_raw.csv"
+download.file(hal_URL, hal_dest, mode = "wb")
 
 
 ## Montreal tree data download
@@ -35,20 +36,24 @@ View(mon_tree_raw)
 
 
 ## Toronto tree data download (shapefile)
+# City of Toronto open data public tree inventory link
 tor_URL<- "https://ckan0.cf.opendata.inter.prod-toronto.ca/download_resource/c1229af1-8ab6-4c71-b131-8be12da59c8e"
-tor_dest<- "/Users/nicoleyu/Desktop/GRI ZULE/R Cross City ES/Raw data/tor_tree_raw.zip"
+# saving to large folder because the shapefile is too big 
+tor_dest<- "large/tor_tree_raw.zip"
 download.file(tor_URL,tor_dest, mode="wb")
-unzip(tor_dest, exdir="/Users/nicoleyu/Desktop/GRI ZULE/Cross City R/GRI Cross City/Raw data/tor_tree_raw")
-tor_tree_raw <- read_sf("/Users/nicoleyu/Desktop/GRI ZULE/Cross City R/GRI Cross City/Raw data/tor_tree_raw/TMMS_Open_Data_WGS84.shp")
+unzip(tor_dest, exdir="large/tor_tree_raw")
+tor_tree_raw <- read_sf("large/tor_tree_raw/TMMS_Open_Data_WGS84.shp")
 
 ## Vancouver tree data download
+# City of Vancouver open data public tree inventory link
 van_URL <- "https://opendata.vancouver.ca/explore/dataset/street-trees/download/?format=csv&timezone=Asia/Shanghai&lang=en&use_labels_for_header=true&csv_separator=%3B"
-van_dest <- "/Users/nicoleyu/Desktop/GRI ZULE/R Cross City ES/Raw data/van_tree_raw.csv"
+van_dest <- "input/van_tree_raw.csv"
 download.file(van_URL,van_dest, mode="wb")
 van_tree_raw <-read.csv(van_dest, sep=";")
 View(van_tree_raw)
 
 ## Winnipeg tree data download
+# City of Winnipeg open data public tree inventory link
 win_URL <- "https://data.winnipeg.ca/api/views/hfwk-jp4h/rows.csv?accessType=DOWNLOAD"
 win_dest <- "/Users/nicoleyu/Desktop/GRI ZULE/R Cross City ES/Raw data/win_tree_raw.csv"
 download.file(win_URL,win_dest, mode="wb")
