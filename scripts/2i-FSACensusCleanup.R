13b98616890887cd15e1eb7c1481b5ce2727080f
Nicole <nicole.yu99@gmail.com>
2021-08-06 23:53:12 +0800

    Corrected DSA Census data cleanup, so the rows are selected by question number instead of the name of the variable, to prevent duplicates

--- scripts/2i-FSACensusCleanup.R
+++ scripts/2i-FSACensusCleanup.R
@@ -2,7 +2,8 @@
 # Author: Nicole Yu & Isabella Richmond
 
 #### Packages ####
-easypackages::packages("tidyverse","sf")
+easypackages::packages("tidyverse","sf","data.table", "bit64")
+install.packages("data.table")
 
 #### Data ####
 # Cleaned canada boundary data
@@ -10,7 +11,7 @@ can_bound <- readRDS("large/MunicipalBoundariesCleaned.rds")
 # Forward sortation area boundaries
 dsa_bound_raw <- read_sf("large/dsa_bound_raw/lda_000b16a_e.shp")
 # 2016  Census survey data
-can_cen_raw <- read.csv("large/can_cen_raw/98-401-X2016044_English_CSV_data.csv")
+can_cen_raw <- fread("large/can_cen_raw/98-401-X2016044_English_CSV_data.csv")
 
 #### Cleanup ####
 ## Dissemination Area boundaries
@@ -27,39 +28,42 @@ dsa_bound <- dsa_bound[can_bound,]
 saveRDS(dsa_bound, "large/DSACleaned.rds")
 
 ## Census survey data
-#  select relevant columns and rows
+# select relevant columns and rows
+# census data for Population 2016, total occupied private dwellings, types of dwellings, 
+# total private households by size, number of people in private households, average household size
+# emploment income, median employment income, and median after-tax income in 2015 among recipients
 can_cen <- can_cen_raw %>%
-  select(c("GEO_CODE..POR.","DIM..Profile.of.Dissemination.Areas..2247.","Dim..Sex..3...Member.ID...1...Total...Sex")) %>%
-  rename(dsa = "GEO_CODE..POR.") %>%
-  rename(sofac = "DIM..Profile.of.Dissemination.Areas..2247.") %>%
-  rename(sonum = "Dim..Sex..3...Member.ID...1...Total...Sex") %>%
-  filter(sofac %in% c("Population, 2016","Total - Occupied private dwellings by structural type of dwelling - 100% data","Single-detached house","Apartment in a building that has five or more storeys","Other attached dwelling","Semi-detached house","Row house","Apartment or flat in a duplex","Apartment in a building that has fewer than five storeys","Other single-attached house","Movable dwelling","Total - Private households by household size - 100% data","1 person","2 persons","3 persons","4 persons","5 or more persons","Number of persons in private households","Average household size","Employment income (%)","Median employment income in 2015 for full-year full-time workers ($)","Median after-tax income in 2015 among recipients ($)"))
+  select(c("GEO_CODE (POR)","Member ID: Profile of Dissemination Areas (2247)","Dim: Sex (3): Member ID: [1]: Total - Sex")) %>%
+  rename(dsa = "GEO_CODE (POR)") %>%
+  rename(sofac = "Member ID: Profile of Dissemination Areas (2247)") %>%
+  rename(sonum = "Dim: Sex (3): Member ID: [1]: Total - Sex") %>%
+  filter(sofac %in% c(1, 41:58, 665, 685, 689))
 # pivot to wide format
 can_cen_wide <- can_cen %>% pivot_wider(names_from = sofac, values_from = sonum)
 # rename columns
 can_cen_wide <- can_cen_wide %>%
-  rename(totpop = "Population, 2016") %>%
-  rename(opdwel =  "Total - Occupied private dwellings by structural type of dwelling - 100% data") %>%
-  rename(sideho = "Single-detached house") %>%
-  rename(aptfiv ="Apartment in a building that has five or more storeys") %>%
-  rename(oadwel = "Other attached dwelling") %>%
-  rename(semhou = "Semi-detached house") %>%
-  rename(rowhou = "Row house") %>%
-  rename(aptdup = "Apartment or flat in a duplex") %>%
-  rename(aptbui = "Apartment in a building that has fewer than five storeys") %>%
-  rename(otsiho = "Other single-attached house") %>%
-  rename(mvdwel = "Movable dwelling") %>%
-  rename(totpri = "Total - Private households by household size - 100% data") %>%
-  rename(prione = "1 person") %>%
-  rename(pritwo = "2 persons") %>%
-  rename(prithr = "3 persons") %>%
-  rename(prifou = "4 persons") %>%
-  rename(prifiv = "5 or more persons") %>%
-  rename(numper = "Number of persons in private households") %>%
-  rename(avhosi = "Average household size") %>%
-  rename(empinc = "Employment income (%)") %>%
-  rename(medemp = "Median employment income in 2015 for full-year full-time workers ($)") %>%
-  rename(medtax = "Median after-tax income in 2015 among recipients ($)")
+  rename(totpop = "1") %>%
+  rename(opdwel = "41") %>%
+  rename(sideho = "42") %>%
+  rename(aptfiv = "43") %>%
+  rename(oadwel = "44") %>%
+  rename(semhou = "45") %>%
+  rename(rowhou = "46") %>%
+  rename(aptdup = "47") %>%
+  rename(aptbui = "48") %>%
+  rename(otsiho = "49") %>%
+  rename(mvdwel = "50") %>%
+  rename(totpri = "51") %>%
+  rename(prione = "52") %>%
+  rename(pritwo = "53") %>%
+  rename(prithr = "54") %>%
+  rename(prifou = "55") %>%
+  rename(prifiv = "56") %>%
+  rename(numper = "57") %>%
+  rename(avhosi = "58") %>%
+  rename(medtax = "665") %>%
+  rename(medemp = "685") %>%
+  rename(empinc = "689")
 # match dissemination area to boundary polygon
 can_cen_dsa<- merge(can_cen_wide, dsa_bound, by = "dsa")
 # transform to sf object
