faa566c1a5a4ea44ccbf3384ac30c2d00dd7c012
Nicole <nicole.yu99@gmail.com>
2021-08-12 22:18:41 +0800

    Added na.rm to mean() and sum() functions, so neighbourhoods with just one dsa won't be returned with NAs

--- scripts/3-CensusDSAToNeighbourhoods.R
+++ scripts/3-CensusDSAToNeighbourhoods.R
@@ -14,7 +14,7 @@ can_cen <- readRDS("large/CensusDSACleaned.rds")
 ## Use a spatially weighted join to get the mean values of each dissemination area within each Calgary neighbourhood
 # find all the DSAs that intersect with each neighbourhood
 hood_cen <- st_intersection(can_hood, can_cen)
- # calculate the area of each DSA, weight of each DSA, and the weighted mean
+# calculate the area of each DSA, weight of each DSA, and the weighted mean
 can_hood_cen <- hood_cen %>%
   select(c("dsa","city","hood","totpop","opdwel","sideho","aptfiv","oadwel","semhou","rowhou","aptdup","aptbui","otsiho","mvdwel",
            "totpri","prione","pritwo","prithr","prifou","prifiv","numper","avhosi","medtax","medemp","empinc")) %>%
@@ -24,33 +24,33 @@ can_hood_cen <- hood_cen %>%
   mutate(wmean = weighted.mean(as.numeric(totpop), as.numeric(area)))
 # make new dataset with one entry per neighbourhood 
 can_hood_sum <- can_hood_cen %>%
-  group_by(hood) %>% 
-  mutate(DSAcount = n()) %>%
-  mutate(weight = sum(weight)) %>%
-  mutate(area = sum(area)) %>%
-  mutate(dsa = list(dsa)) %>% 
-  mutate(totpop = sum(as.numeric(totpop))) %>%
-  mutate(opdwel = mean(as.numeric(opdwel))) %>%
-  mutate(sideho = mean(as.numeric(sideho))) %>%
-  mutate(aptfiv = mean(as.numeric(aptfiv))) %>%
-  mutate(oadwel = mean(as.numeric(oadwel))) %>%
-  mutate(semhou = mean(as.numeric(semhou))) %>%
-  mutate(rowhou = mean(as.numeric(rowhou))) %>%
-  mutate(aptdup = mean(as.numeric(aptdup))) %>%
-  mutate(aptbui = mean(as.numeric(aptbui))) %>%
-  mutate(otsiho = mean(as.numeric(otsiho))) %>%
-  mutate(mvdwel = mean(as.numeric(mvdwel))) %>%
-  mutate(totpri = mean(as.numeric(totpri))) %>%
-  mutate(prione = mean(as.numeric(prione))) %>%
-  mutate(pritwo = mean(as.numeric(pritwo))) %>%
-  mutate(prithr = mean(as.numeric(prithr))) %>%
-  mutate(prifou = mean(as.numeric(prifou))) %>%
-  mutate(prifiv = mean(as.numeric(prifiv))) %>%
-  mutate(numper = mean(as.numeric(numper))) %>%
-  mutate(avhosi = mean(as.numeric(avhosi))) %>%
-  mutate(medtax = median(as.numeric(medtax))) %>%
-  mutate(medemp = median(as.numeric(medemp))) %>%
-  mutate(empinc = median(as.numeric(empinc))) %>%
+  dplyr::group_by(hood) %>%   
+  mutate(DSAcount = n(),
+         weight = sum(weight),
+         area = sum(area),
+         dsa = list(dsa),
+         totpop = sum(as.numeric(totpop)),
+         opdwel = mean(as.numeric(opdwel)),
+         sideho = mean(as.numeric(sideho)),
+         aptfiv = mean(as.numeric(aptfiv)),
+         oadwel = mean(as.numeric(oadwel)),
+         semhou = mean(as.numeric(semhou)),
+         rowhou = mean(as.numeric(rowhou)),
+         aptdup = mean(as.numeric(aptdup)),
+         aptbui = mean(as.numeric(aptbui)),
+         otsiho = mean(as.numeric(otsiho)),
+         mvdwel = mean(as.numeric(mvdwel)),
+         totpri = mean(as.numeric(totpri)),
+         prione = mean(as.numeric(prione)),
+         pritwo = mean(as.numeric(pritwo)),
+         prithr = mean(as.numeric(prithr)),
+         prifou = mean(as.numeric(prifou)),
+         prifiv = mean(as.numeric(prifiv)),
+         numper = mean(as.numeric(numper)),
+         avhosi = mean(as.numeric(avhosi)),
+         medtax = median(as.numeric(medtax)),
+         medemp = median(as.numeric(medemp)),
+         empinc = median(as.numeric(empinc))) %>%
   distinct(hood, .keep_all = TRUE)
 # save
 saveRDS(can_hood_sum, "large/NeighbourhoodsCensusSum.rds")
