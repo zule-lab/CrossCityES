cb79fbefaf156f380260907645b12738226916dc
icrichmond <icrichmond@mun.ca>
2021-09-09 15:13:50 -0400

    Formatting

--- scripts/2b-NeighbourhoodsCleanup.R
+++ scripts/2b-NeighbourhoodsCleanup.R
@@ -43,7 +43,6 @@ cal_hood <- cal_hood_raw %>%
   mutate(city = c("Calgary")) # select neighbourhood codes and names columns and rename
 cal_hood <- st_as_sf(cal_hood, wkt = "geometry", crs = 4326)
 cal_hood <- hood_cleaned(cal_hood, cal, "large/neighbourhoods/CalgaryNeighbourhoodsCleaned.rds")
-
 # Halifax
 hal_hood <- hal_hood_raw %>% 
   select(c("GSA_NAME", "geometry")) %>% 
@@ -51,32 +50,27 @@ hal_hood <- hal_hood_raw %>%
   filter(hood == "HALIFAX") %>%
   mutate(city = c("Halifax")) # Halifax peninsula is considered to be one neighbourhood as per correspondence with city officials
 hal_hood <- hood_cleaned(hal_hood, hal, "large/neighbourhoods/HalifaxNeighbourhoodsCleaned.rds")
-
 # Montreal
 mon_hood <- mon_hood_raw %>% select(c("NOM","geometry")) %>%
   rename("hood" = "NOM") %>%
   mutate(city = c("Montreal"))
 mon_hood <- hood_cleaned(mon_hood, mon, "large/neighbourhoods/MontrealNeighbourhoodsCleaned.rds")
-
 # Ottawa
 ott_hood <- ott_hood_raw %>% 
   select(c("Name", "geometry")) %>% 
   rename("hood" = "Name") %>%
   mutate(city = c("Ottawa"))
 ott_hood <- hood_cleaned(ott_hood, ott, "large/neighbourhoods/OttawaNeighbourhoodsCleaned.rds")
-
 # Toronto
 tor_hood <- tor_hood_raw %>% select(c("FIELD_8", "geometry")) %>% 
   rename("hood" = "FIELD_8")%>%
   mutate(city = c("Toronto"))
 tor_hood <- hood_cleaned(tor_hood, tor, "large/neighbourhoods/TorontoNeighbourhoodsCleaned.rds")
-
 # Vancouver
 van_hood <- van_hood_raw %>% select(c("name","geometry"))%>%
   rename(hood = "name") %>%
   mutate(city = c("Vancouver"))
 van_hood <- hood_cleaned(van_hood, van, "large/neighbourhoods/VancouverNeighbourhoodsCleaned.rds")
-
 # Winnipeg
 win_hood <- win_hood_raw %>% select(c("name","geometry")) %>%
   rename(hood = "name") %>%
