c2d1df0e0cca7ecf868384adb32d5a9047c38f8f
icrichmond <icrichmond@mun.ca>
2021-09-10 17:47:28 -0400

    working on function

--- scripts/4a-CalgaryCleanup.R
+++ scripts/4a-CalgaryCleanup.R
@@ -4,24 +4,66 @@
 # This script is for cleaning the Calgary public tree inventory
 # Still require street, neighbourhood, street, and park/street tree columns
 
-#### Packages #### 
+#### PACKAGES #### 
 # load packages 
-easypackages::packages("sf", "tidyverse")
+p <- c("sf", "dplyr", "readr")
+lapply(p, library, character.only = T)
 
-#### Data ####
+#### FUNCTIONS ####
+tree_cleaning <- function(city, trees, parks, hoods, boundaries, roads){
+  p <- c("sf", "dplyr", "tidyr")
+  lapply(p, library, character.only = T)
+  
+  parks <- st_transform(parks, crs = 3347)
+  trees <- st_transform(trees, crs = 3347)
+  saveRDS(parks, paste0("large/parks/", city, "ParksCleaned.rds"))
+  
+  city_bound <- subset(boundaries, bound == "city")
+  city_road <- roads[city_bound,]
+  city_road <- select(city_road, c("street", "streetid", "geometry"))
+  city_road <- city_road %>% mutate(index = rownames(city_road))
+  saveRDS(city_road, paste0("large/national/", city, "RoadsCleaned.rds"))
+  
+  trees <- st_join(trees, hoods, join = st_intersects)
+  trees <- st_join(trees, parks, join = st_intersects)
+  
+  dup <- trees$id[duplicated(trees$id)]
+  trees <- trees %>% filter(!id %in% dup)
+  # replace NAs with "no" to indicate street trees 
+  trees$park %>% replace_na("no")
+  # if value is not "no", change value to "yes" so park column is binary yes/no
+  trees$park[trees$park != "no"] <- "yes"
+  # st_nearest_feature returns the index value not the street name
+  #trees$streetid <- st_nearest_feature(trees, city_road)
+  # return unique streetid based on index values
+  #trees$streetid <- city_road$streetid[match(as.character(trees$streetid), as.character(trees$index))]
+  # add column with street name
+  #trees$street <- trees$street[match(as.character(trees$streetid), as.character(trees$streetid))]
+  #trees <- trees %>% filter(park == "no")
+  
+  #trees <- trees[city_bound,]
+  
+  # reorder columns
+  #trees <- trees[,c("city","id","genus", "species", "cultivar", "geometry","hood","streetid","park", "street", "dbh")]# Check and make output
+  
+  #saveRDS(trees,  paste0("large/trees/", city, "TreesCleaned.rds"))
+}
+
+
+#### DATA ####
 # load data downloaded in 1-DataDownload.R
 # tree inventory
-cal_tree_raw <- read_csv("large/cal_tree_raw.csv")
+cal_tree_raw <- read_csv("large/trees/cal_tree_raw.csv")
 # parks
-cal_park_raw <- read_csv("input/cal_park_raw.csv")
+cal_park_raw <- read_csv("large/parks/cal_park_raw.csv")
 # neighbourhoods 
-cal_hood <- readRDS("large/CalgaryNeighbourhoodsCleaned.rds")
+cal_hood <- readRDS("large/neighbourhoods/CalgaryNeighbourhoodsCleaned.rds")
 # municipal boundaries 
-can_bound <- readRDS("large/MunicipalBoundariesCleaned.rds")
+can_bound <- readRDS("large/national/MunicipalBoundariesCleaned.rds")
 # roads
-can_road <- readRDS("large/RoadsCleaned.rds")
+can_road <- readRDS("large/national/RoadsCleaned.rds")
 
-#### Data Cleaning ####
+#### CLEANING ####
 ## Parks
 # select relevant columns and rename 
 cal_park <- cal_park_raw %>%
@@ -30,9 +72,6 @@ cal_park <- cal_park_raw %>%
   rename("geometry" = "the_geom")
 # convert to sf object 
 cal_park <- st_as_sf(cal_park, wkt = "geometry", crs = 4326)
-cal_park <- st_transform(cal_park, crs = 6624)
-# save
-saveRDS(cal_park, "large/CalgaryParksCleaned.rds")
 
 ## Trees
 # check for dupes
@@ -53,56 +92,7 @@ cal_tree$cultivar <- substr(cal_tree$cultivar,2,nchar(cal_tree$cultivar)-1)
 cal_tree <- drop_na(cal_tree, c(latitude,longitude))
 # convert to sf object 
 cal_tree <- st_as_sf(cal_tree, coords = c("longitude", "latitude"), crs = 4326)
-# transform
-cal_tree <- st_transform(cal_tree, crs = 6624)
-
-## City Boundary & Roads 
-# select Calgary boundary
-cal_bound <- subset(can_bound, bound == "Calgary")
-# select roads within the Calgary boundary
-cal_road <- can_road[cal_bound,]
-cal_road <- st_transform(cal_road, crs = 6624)
-cal_road <- select(cal_road, c("street","streetid", "geometry"))
-#add row index numbers as a column for recoding later
-cal_road <- cal_road %>% mutate(index= 1:n())
-#save
-saveRDS(cal_road, "large/CalgaryRoadsCleaned.rds")
-
-#### Spatial Joins ####
-## Neighbourhoods
-# want to add columns that specifies what city and neighbourhood each tree belongs to
-# join trees and neighbourhoods using st_intersects
-cal_tree <- st_join(cal_tree, cal_hood, join = st_intersects)
-## Parks 
-# want to identify which trees are park trees and which are street 
-cal_tree <- st_join(cal_tree, cal_park, join = st_intersects)
-# remove all trees on polygon boundaries after joining with parks by deleting duplicates
-cal_dupe <- cal_tree$id[duplicated(cal_tree$id)]
-cal_tree <- cal_tree %>% filter(!id %in% cal_dupe)
-# replace NAs with "no" to indicate street trees 
-cal_tree <- replace_na(cal_tree, list(park = "no"))
-# if value is not "no", change value to "yes" so park column is binary yes/no
-cal_tree$park[cal_tree$park != "no"] <- "yes"
-## Streets
-# st_nearest_feature returns the index value not the street name
-cal_tree$streetid <- st_nearest_feature(cal_tree, cal_road)
-# return unique streetid based on index values
-cal_tree$streetid <- cal_road$streetid[match(as.character(cal_tree$streetid), as.character(cal_road$index))]
-# add column with street name
-cal_tree$street <- cal_road$street[match(as.character(cal_tree$streetid), as.character(cal_road$streetid))]
 
 
-#### Remove park trees ####
-cal_tree <- cal_tree %>% filter(park == "no")
+tree_cleaning("Calgary", cal_tree, cal_park, cal_hood, can_bound, can_road)
 
-#### Remove trees with incorrect coordinates ####
-# some trees may have coordinates that place them outside the city's boundaries
-# remove the erroneous trees using spatial join
-cal_tree <- cal_tree[cal_bound,]
-
-#### Save ####
-# reorder columns
-cal_tree <- cal_tree[,c("city","id","genus", "species", "cultivar", "geometry","hood","streetid","street","park","dbh")]# Check and make output
-# save cleaned Calgary tree dataset as rds and shapefile
-saveRDS(cal_tree, "large/CalgaryTreesCleaned.rds")
-st_write(cal_tree, "large/CalgaryTreesCleaned.gpkg", driver="GPKG")
