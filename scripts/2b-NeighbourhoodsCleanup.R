79f4ef0a377cfed2dde5ae4cf10d9659d03edb69
icrichmond <icrichmond@mun.ca>
2021-09-10 16:13:25 -0400

    Function typo for hood_id

--- scripts/2b-NeighbourhoodsCleanup.R
+++ scripts/2b-NeighbourhoodsCleanup.R
@@ -9,7 +9,7 @@ lapply(p, library, character.only = T)
 hood_cleaned <- function(df, prefix, dest){
   p <- c("dplyr", "stringr", "sf")
   lapply(p, library, character.only = T)
-  df %>% mutate(hood_id = paste0("prefix", seq.int(nrow(df)))) # add hood id
+  df <- df %>% mutate(hood_id = paste0(prefix, seq.int(nrow(df)))) # add hood id
   df$hood <- str_to_title(df$hood)   # change case of hood names
   df <- st_transform(df, crs = 3347) # transform
   df$hood_area <- st_area(df) # add area column
@@ -42,40 +42,40 @@ cal_hood <- cal_hood_raw %>%
   rename("geometry" = "the_geom") %>%
   mutate(city = c("Calgary")) # select neighbourhood codes and names columns and rename
 cal_hood <- st_as_sf(cal_hood, wkt = "geometry", crs = 4326)
-cal_hood <- hood_cleaned(cal_hood, cal, "large/neighbourhoods/CalgaryNeighbourhoodsCleaned.rds")
+cal_hood <- hood_cleaned(cal_hood, "cal", "large/neighbourhoods/CalgaryNeighbourhoodsCleaned.rds")
 # Halifax
 hal_hood <- hal_hood_raw %>% 
   select(c("GSA_NAME", "geometry")) %>% 
   rename("hood" = "GSA_NAME") %>% 
   filter(hood == "HALIFAX") %>%
   mutate(city = c("Halifax")) # Halifax peninsula is considered to be one neighbourhood as per correspondence with city officials
-hal_hood <- hood_cleaned(hal_hood, hal, "large/neighbourhoods/HalifaxNeighbourhoodsCleaned.rds")
+hal_hood <- hood_cleaned(hal_hood, "hal", "large/neighbourhoods/HalifaxNeighbourhoodsCleaned.rds")
 # Montreal
 mon_hood <- mon_hood_raw %>% select(c("NOM","geometry")) %>%
   rename("hood" = "NOM") %>%
   mutate(city = c("Montreal"))
-mon_hood <- hood_cleaned(mon_hood, mon, "large/neighbourhoods/MontrealNeighbourhoodsCleaned.rds")
+mon_hood <- hood_cleaned(mon_hood, "mon", "large/neighbourhoods/MontrealNeighbourhoodsCleaned.rds")
 # Ottawa
 ott_hood <- ott_hood_raw %>% 
   select(c("Name", "geometry")) %>% 
   rename("hood" = "Name") %>%
   mutate(city = c("Ottawa"))
-ott_hood <- hood_cleaned(ott_hood, ott, "large/neighbourhoods/OttawaNeighbourhoodsCleaned.rds")
+ott_hood <- hood_cleaned(ott_hood, "ott", "large/neighbourhoods/OttawaNeighbourhoodsCleaned.rds")
 # Toronto
 tor_hood <- tor_hood_raw %>% select(c("FIELD_8", "geometry")) %>% 
   rename("hood" = "FIELD_8")%>%
   mutate(city = c("Toronto"))
-tor_hood <- hood_cleaned(tor_hood, tor, "large/neighbourhoods/TorontoNeighbourhoodsCleaned.rds")
+tor_hood <- hood_cleaned(tor_hood, "tor", "large/neighbourhoods/TorontoNeighbourhoodsCleaned.rds")
 # Vancouver
 van_hood <- van_hood_raw %>% select(c("name","geometry"))%>%
   rename(hood = "name") %>%
   mutate(city = c("Vancouver"))
-van_hood <- hood_cleaned(van_hood, van, "large/neighbourhoods/VancouverNeighbourhoodsCleaned.rds")
+van_hood <- hood_cleaned(van_hood, "van", "large/neighbourhoods/VancouverNeighbourhoodsCleaned.rds")
 # Winnipeg
 win_hood <- win_hood_raw %>% select(c("name","geometry")) %>%
   rename(hood = "name") %>%
   mutate(city = c("Winnipeg"))
-win_hood <- hood_cleaned(win_hood, win, "large/neighbourhoods/WinnipegNeighbourhoodsCleaned.rds")
+win_hood <- hood_cleaned(win_hood, "win", "large/neighbourhoods/WinnipegNeighbourhoodsCleaned.rds")
 
 #### COMBINE ####
 can_hood <- rbind(cal_hood, hal_hood, mon_hood, ott_hood, tor_hood, van_hood, win_hood)
