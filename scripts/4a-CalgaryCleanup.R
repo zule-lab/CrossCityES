b50363d878698f2a6df72a5ca08d0b99f6f2e5d9
icrichmond <icrichmond@mun.ca>
2021-09-14 15:44:30 -0400

    updated with function

--- scripts/4a-CalgaryCleanup.R
+++ scripts/4a-CalgaryCleanup.R
@@ -2,53 +2,14 @@
 # Author: Nicole Yu
 
 # This script is for cleaning the Calgary public tree inventory
-# Still require street, neighbourhood, street, and park/street tree columns
 
 #### PACKAGES #### 
 # load packages 
-p <- c("sf", "dplyr", "readr")
+p <- c("sf", "dplyr", "tidyr", "readr")
 lapply(p, library, character.only = T)
 
 #### FUNCTIONS ####
-tree_cleaning <- function(city, trees, parks, hoods, boundaries, roads){
-  p <- c("sf", "dplyr", "tidyr")
-  lapply(p, library, character.only = T)
-  
-  parks <- st_transform(parks, crs = 3347)
-  trees <- st_transform(trees, crs = 3347)
-  saveRDS(parks, paste0("large/parks/", city, "ParksCleaned.rds"))
-  
-  city_bound <- subset(boundaries, bound == city)
-  city_road <- roads[city_bound,]
-  city_road <- select(city_road, c("street", "streetid", "geometry"))
-  city_road <- city_road %>% mutate(index = 1:n())
-  saveRDS(city_road, paste0("large/national/", city, "RoadsCleaned.rds"))
-  
-  trees <- st_join(trees, hoods, join = st_intersects)
-  trees <- st_join(trees, parks, join = st_intersects)
-  
-  dup <- trees$id[duplicated(trees$id)]
-  trees <- trees %>% filter(!id %in% dup)
-  # replace NAs with "no" to indicate street trees 
-  trees <- mutate(trees, park = replace_na(park, "no"))
-  # if value is not "no", change value to "yes" so park column is binary yes/no
-  trees <- mutate(trees, park = ifelse(park == "no", "no", "yes"))
-  # st_nearest_feature returns the index value not the street name
-  trees <- mutate(trees, streetid = st_nearest_feature(trees, city_road))
-  # return unique streetid based on index values
-  #trees <- mutate(trees, streetid = city_road$streetid[match(as.character(trees$streetid), as.character(city_road$index))])
-  # add column with street name
-  #trees <- mutate(trees, street = match(as.character(trees$streetid), as.character(city_road$streetid)))
-  
-  trees <- trees %>% filter(park == "no")
-  
-  trees <- trees[city_bound,]
-  
-  # reorder columns
-  trees <- trees[,c("city","id","genus", "species", "cultivar", "geometry","hood","streetid","park", "street", "dbh")]# Check and make output
-  
-  saveRDS(trees,  paste0("large/trees/", city, "TreesCleaned.rds"))
-}
+source("scripts/function-TreeCleaning.R")
 
 #### DATA ####
 # load data downloaded in 1-DataDownload.R
@@ -92,7 +53,6 @@ cal_tree$cultivar <- substr(cal_tree$cultivar,2,nchar(cal_tree$cultivar)-1)
 cal_tree <- drop_na(cal_tree, c(latitude,longitude))
 # convert to sf object 
 cal_tree <- st_as_sf(cal_tree, coords = c("longitude", "latitude"), crs = 4326)
-
 
 ## Final Dataset
 t <- tree_cleaning("Calgary", cal_tree, cal_park, cal_hood, can_bound, can_road)
