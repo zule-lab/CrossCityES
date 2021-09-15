e54cf55cacb1d9234f1dbd1e666c3c856d484b79
icrichmond <icrichmond@mun.ca>
2021-09-15 13:29:12 -0400

    updated with function

--- scripts/4g-WinnipegCleanup.R
+++ scripts/4g-WinnipegCleanup.R
@@ -2,22 +2,33 @@
 # Author: Nicole Yu
 
 # This script is for cleaning the Winnipeg public tree inventory
-# Winnipeg dataset has neighbourhood, street and park/street column
 
-#### Packages #### 
+#### PACKAGES #### 
 # load packages 
-easypackages::packages("sf", "tidyverse")
+p <- c("sf", "dplyr", "tidyr", "readr")
+lapply(p, library, character.only = T)
 
-#### Data ####
-# load data downloaded in 1-DataDownload.R
+#### FUNCTIONS ####
+source("scripts/function-TreeCleaning.R")
+
+#### DATA ####
 # tree inventory
-win_tree_raw <-read.csv("large/win_tree_raw.csv")
+win_tree_raw <- read_csv("large/trees/win_tree_raw.csv")
+# parks 
+win_park_raw <- st_read(file.path("/vsizip", "large/parks/win_park_raw.zip"))
 # neighbourhoods
-win_hood <- readRDS("large/WinnipegNeighbourhoodsCleaned.rds")
+win_hood <- readRDS("large/neighbourhoods/WinnipegNeighbourhoodsCleaned.rds")
 # municipal boundaries 
-can_bound <- readRDS("large/MunicipalBoundariesCleaned.rds")
+can_bound <- readRDS("large/national/MunicipalBoundariesCleaned.rds")
+# roads 
+can_road <- readRDS("large/national/RoadsCleaned.rds")
 
 #### Data Cleaning ####
+## Parks 
+win_park <- win_park_raw %>%
+  select(c("park_name", "geometry")) %>%
+  rename("park" = "park_name")
+
 ## Trees
 # check for dupes
 unique(duplicated(win_tree_raw$tree_id))
@@ -25,8 +36,6 @@ unique(duplicated(win_tree_raw$tree_id))
 win_tree <- win_tree_raw %>%
   select(c("the_geom","tree_id","botanical","dbh","park","street")) %>%
   rename("id" = "tree_id")
-# add streetid column to match other cities
-win_tree$streetid <- c(NA)
 # sorting species name into genus, species, and cultivar columns
 win_tree <- win_tree %>% separate(botanical, c("genus","species","var","cultivar"))
 # assign blanks and NAs in species column to "sp."
@@ -46,28 +55,6 @@ win_tree <- separate(data = win_tree, col = the_geom, into = c("lat", "long"), s
 win_tree <- drop_na(win_tree, c(lat,long))
 # convert to sf object 
 win_tree <- st_as_sf(win_tree, coords = c("lat", "long"), crs = 4326)
-# transform
-win_tree <- st_transform(win_tree, crs = 6624)
 
-#### Spatial Joins ####
-## Neighbourhoods
-# want to add columns that specifies what city and neighbourhood each tree belongs to
-# join trees and neighbourhoods using st_intersects
-win_tree <- st_join(win_tree, win_hood, join = st_intersects)
-
-#### Remove park trees ####
-win_tree <- win_tree %>% filter(park == "no")
-
-#### Remove trees with incorrect coordinates ####
-# some trees may have coordinates that place them outside the city's boundaries
-# select Winnipeg boundary
-win_bound <- subset(can_bound, bound == "Winnipeg")
-# remove the erroneous trees using spatial join
-win_tree <- win_tree[win_bound,]
-
-#### Save ####
-# reorder columns
-win_tree <- win_tree[,c("city","id","genus","species","cultivar","geometry","hood","streetid","street","park","dbh")]
-# save cleaned Winnipeg tree dataset as rds and shapefile
-saveRDS(win_tree, "large/WinnipegTreesCleaned.rds")
-st_write(win_tree, "large/WinnipegTreesCleaned.gpkg", driver = "GPKG")
+## Final Dataset 
+tree_cleaning("Winnipeg", win_tree, win_park, win_hood, can_bound, can_road)
