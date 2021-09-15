387d454060f2e38dfc75817c003a8689251731b7
icrichmond <icrichmond@mun.ca>
2021-09-15 13:22:23 -0400

    updated with function

--- scripts/4f-VancouverCleanup.R
+++ scripts/4f-VancouverCleanup.R
@@ -2,34 +2,33 @@
 # Author: Nicole Yu
 
 # This script is for cleaning the Vancouver public tree inventory
-# Vancouver dataset has neighbourhood and street
-# Still require park/street column
 
-#### Packages #### 
+#### PACKAGES #### 
 # load packages 
-easypackages::packages("sf", "tidyverse")
+p <- c("sf", "dplyr", "tidyr", "stringr")
+lapply(p, library, character.only = T)
 
-#### Data ####
-# load data downloaded in 1-DataDownload.R
+#### FUNCTIONS ####
+source("scripts/function-TreeCleaning.R")
+
+#### DATA ####
 # tree inventory
-van_tree_raw <-read.csv("input/van_tree_raw.csv", sep=";")
+van_tree_raw <- read.csv("large/trees/van_tree_raw.csv", sep=";")
 # parks
-van_park_raw <- read_sf("large/van_park_raw/parks-polygon-representation.shp")
+van_park_raw <- st_read(file.path("/vsizip", "large/parks/van_park_raw.zip"))
 # neighbourhoods
-van_hood <- readRDS("large/VancouverNeighbourhoodsCleaned.rds")
+van_hood <- readRDS("large/neighbourhoods/VancouverNeighbourhoodsCleaned.rds")
 # municipal boundaries 
-can_bound <- readRDS("large/MunicipalBoundariesCleaned.rds")
+can_bound <- readRDS("large/national/MunicipalBoundariesCleaned.rds")
+# roads
+can_road <- readRDS("large/national/RoadsCleaned.rds")
 
-#### Data Cleaning ####
+#### CLEANING ####
 ## Parks
 # select relevant columns and rename 
 van_park <- van_park_raw %>%
   select(c("park_name", "geometry")) %>%
   rename("park" = "park_name")
-# transform to ESPG: 6624
-van_park <- st_transform(van_park, crs = 6624)
-# save 
-saveRDS(van_park, "large/VancouverParksCleaned.rds")
 
 ## Trees
 # check for dupes
@@ -43,14 +42,11 @@ van_tree <- van_tree_raw %>%
   rename("cultivar" = "CULTIVAR_NAME") %>%
   rename("street" = "ON_STREET") %>%
   rename("dbh" = "DIAMETER")
-# add streetid column to match other cities
-van_tree$streetid <- c(NA)
 # change case of characters
 van_tree$genus <- str_to_title(van_tree$genus) 
 van_tree$species <- tolower(van_tree$species) 
 van_tree$cultivar <- str_to_title(van_tree$cultivar) 
 van_tree$street <- str_to_title(van_tree$street) 
-van_tree$hood <- str_to_title(van_tree$hood) 
 # change "species in species" column to "sp."
 van_tree$species[van_tree$species == "species"] <- "sp."
 # convert empty as NA for cultivar
@@ -60,41 +56,9 @@ van_tree$dbh <- van_tree$dbh*2.54
 # data is formatted as GeoJSON
 van_tree$Geom <- substr(van_tree$Geom,35,nchar(van_tree$Geom)-2)
 van_tree <- separate(data = van_tree, col = Geom, into = c("long", "lat"), sep = "\\, ")
-van_tree <- st_as_sf(x = van_tree, coords = c("long", "lat"), crs = 4326, na.fail = FALSE, remove = FALSE)
 # drop any rows that have NA lat/long
 van_tree <- drop_na(van_tree, c(lat,long))
-# transform
-van_tree <- st_transform(van_tree, crs = 6624)
+van_tree <- st_as_sf(x = van_tree, coords = c("long", "lat"), crs = 4326, na.fail = FALSE, remove = FALSE)
 
-#### Spatial Joins ####
-## Neighbourhoods
-# want to add columns that specifies what city and neighbourhood each tree belongs to
-# join trees and neighbourhoods using st_intersects
-van_tree <- st_join(van_tree, van_hood, join = st_intersects)
-## Parks 
-# want to identify which trees are park trees and which are street 
-van_tree <- st_join(van_tree, van_park, join = st_intersects)
-# remove all trees on polygon boundaries after joining with parks by deleting duplicates
-van_dupe <- van_tree$id[duplicated(van_tree$id)]
-van_tree <- van_tree %>% filter(!id %in% van_dupe)
-# replace NAs with "no" to indicate street trees 
-van_tree <- replace_na(van_tree, list(park = "no"))
-# if value is not "no", change value to "yes" so park column is binary yes/no
-van_tree$park[van_tree$park != "no"] <- "yes"
-
-#### Remove park trees ####
-van_tree <- van_tree %>% filter(park == "no")
-
-#### Remove trees with incorrect coordinates ####
-# some trees may have coordinates that place them outside the city's boundaries
-# select Vancouver boundary
-van_bound <- subset(can_bound, bound == "Vancouver")
-# remove the erroneous trees using spatial join
-van_tree <- van_tree[van_bound,]
-
-#### Save ####
-# reorder columns
-van_tree <- van_tree[,c("city","id","genus", "species", "cultivar", "geometry","hood","streetid","street","park","dbh")]# Check and make output
-# save cleaned Vancouver tree dataset as rds and shapefile
-saveRDS(van_tree, "large/VancouverTreesCleaned.rds")
-st_write(van_tree, "large/VancouverTreesCleaned.gpkg", driver = "GPKG")
+## Final Dataset ## 
+tree_cleaning("Vancouver", van_tree, van_park, van_hood, can_bound, can_road)
