b8046e90f1e58d5649b972ada10a5f4eb8270bb3
icrichmond <icrichmond@mun.ca>
2021-09-21 09:57:47 -0400

    Ottawa tree species codes

--- scripts/4d-OttawaCleanup.R
+++ scripts/4d-OttawaCleanup.R
@@ -14,6 +14,8 @@ source("scripts/function-TreeCleaning.R")
 #### DATA ####
 # tree inventory
 ott_tree_raw <- read_csv("large/trees/ott_tree_raw.csv")
+# tree species codes 
+ott_tree_spcode <- read_csv("input/ott_tree_spcode.csv")
 # parks
 ott_park_raw <- st_read(file.path("/vsizip", "large/parks/ott_park_raw.zip"))
 # neighbourhoods 
@@ -41,11 +43,28 @@ ott_tree <- ott_tree_raw %>%
   rename("lat" = "Y") %>%
   rename("id" = "OBJECTID") %>%
   rename("street" = "ADDSTR") %>%
-  rename("species" = "SPECIES") %>%
   rename("dbh" = "DBH")
-# no scientific names yet so just add empty genus and cultivar columns 
-ott_tree$genus <- c(NA)
-ott_tree$cultivar <- c(NA)
+# match scientific names using species codes
+ott_tree$SPECIES <- ott_tree_spcode$SPECIES_LATIN[match(as.character(ott_tree$SPECIES), as.character(ott_tree_spcode$SPECIES_COMMON))]
+# rename problematic species 
+ott_tree$SPECIES[ott_tree$SPECIES == "Hedge Start"] <- "Hedge sp."
+ott_tree$SPECIES[ott_tree$SPECIES == "Hedge End"] <- "Hedge sp."
+ott_tree$SPECIES[ott_tree$SPECIES == "Other - See Notes"] <- "Unknown sp."
+# sorting species name into genus, species, and cultivar columns
+ott_tree <- ott_tree %>% separate(SPECIES, c("genus","species","var","cultivar", "cultivar2"))
+ott_tree$species[ott_tree$species == "species"] <- "sp."
+ott_tree$species[ott_tree$species == "Species"] <- "sp."
+ott_tree$species[ott_tree$species == "sp"] <- "sp."
+ott_tree$species[ott_tree$species == "X"] <- "x"
+ott_tree$species[ott_tree$species == "crabapple"] <- "sp."
+ott_tree$species[ott_tree$species == "apple"] <- "sp."
+ott_tree$cultivar[ott_tree$cultivar == "Species"] <- NA
+ott_tree$var[ott_tree$var == "var"] <- NA
+ott_treecul <- ott_tree %>% filter(species != "x") %>% unite(cultivar, c("var", "cultivar", "cultivar2"), na.rm = TRUE, sep = " ")%>%
+  mutate(cultivar = na_if(cultivar, ""))
+ott_treesp <- ott_tree %>% filter(species == "x") %>% unite(species, c("species", "var"), na.rm = TRUE, sep = " ") %>% unite(cultivar, c("cultivar", "cultivar2"), na.rm = TRUE, sep = " ")%>%
+  mutate(cultivar = na_if(cultivar, ""))
+ott_tree <- rbind(ott_treecul, ott_treesp)
 # drop any rows that have NA lat/long
 ott_tree <- drop_na(ott_tree, c(lat,long))
 # adding geometry column and specify projection outlined in City of Ottawa metadata 
