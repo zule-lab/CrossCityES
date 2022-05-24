bf091e4c948677e1a91f7fa99558b0d18a82305e
icrichmond <icrichmond@mun.ca>
2022-05-24 14:02:07 -0400

    download shp function

---
+++ R/download_shp.R
@@ -0,0 +1,11 @@
+download_shp <- function(url, dest){
+  
+  temp <- tempfile()
+  
+  download.file(url, dest, mode = "wb")
+  
+  shp <- st_read(file.path("/vsizip", dest))
+  
+  View(shp)
+  
+}
