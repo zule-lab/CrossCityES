fe88dc4faf6c71a8ad317cf90fe7babb33bb4f91
icrichmond <icrichmond@mun.ca>
2022-05-25 10:27:59 -0400

    return instead of saveRDS

--- R/building_cleanup.R
+++ R/building_cleanup.R
@@ -5,7 +5,7 @@ building_cleanup <- function(geo, city, boundary){
   geo <- st_transform(geo, crs = 3347)
   geo <- geo %>% mutate(city = c(city))
   geo_build <- geo[boundary,]
-  saveRDS(geo_build, dest)
-  readRDS(dest)
+  
+  return(geo_build)
   
 }
