f4fe6bbccb9755e5035c76533ed507a8dc225877
icrichmond <icrichmond@mun.ca>
2022-05-30 16:56:02 -0400

    return df

--- R/convert_area.R
+++ R/convert_area.R
@@ -2,4 +2,6 @@ convert_area <- function(can_build){
   # set units as km^2
   units(can_build$build_area) <- make_units(km^2)
   units(can_build$hood_area) <- make_units(km^2)
+  
+  return(can_build)
 }
