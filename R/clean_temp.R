870a3649d4cc116caa24a2bff7b2b475e0a17aef
icrichmond <icrichmond@mun.ca>
2022-11-14 11:57:16 -0500

    change temp clean based on new temp data

--- R/clean_temp.R
+++ R/clean_temp.R
@@ -1,11 +1,6 @@
 clean_temp <- function(df){
 
-  # need to extract dates from the system.index column 
-  df$date <- sapply(strsplit(df$system.index, "_"), function(x) x[3])
-  df$date <- anydate(df$date)
-  # need to extract image id from system.index without long series of 0s at the end
-  df$id <- sub("_[^_]+$", "", df$system.index) 
-  # drop system.index column - no longer useful 
-  df <- subset(df, select = -system.index)
+  # remove system:index and geo columns because they are not in useable format
+  df <- df %>% select(-c('system.index', '.geo'))
   
 }
