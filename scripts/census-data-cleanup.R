9fc69990f7639949ae281a8fee6edf409b8ee90b
icrichmond <icrichmond@mun.ca>
2022-11-16 15:05:14 -0500

    updating for 2021 census + adding cma

--- scripts/census-data-cleanup.R
+++ scripts/census-data-cleanup.R
@@ -1,7 +1,7 @@
 census_data_cleanup <- c(
   
   tar_target(
-    DA_s,
+    da_s,
     DA_raw %>%
       select(c("DAUID", "geometry")) %>%
       rename(dsa = "DAUID") %>%
@@ -9,99 +9,179 @@ census_data_cleanup <- c(
   ),
   
   tar_target(
-    DA_bound, 
-    DA_s[mun_bound,]
+    da_bound, 
+    da_s[mun_bound,]
   ),
   
   tar_target(
-    census_f,
+    census_da_f,
     census_raw %>%
-      select(c("GEO_CODE (POR)","Member ID: Profile of Dissemination Areas (2247)","Dim: Sex (3): Member ID: [1]: Total - Sex")) %>%
-      rename(dsa = "GEO_CODE (POR)") %>%
-      rename(sofac = "Member ID: Profile of Dissemination Areas (2247)") %>%
-      rename(sonum = "Dim: Sex (3): Member ID: [1]: Total - Sex") %>%
-      filter(sofac %in% c(1, 6:7, 42:50, 665, 857, 1149, 1290, 1324, 1692))
+      select(c("ALT_GEO_CODE","CHARACTERISTIC_ID","C1_COUNT_TOTAL")) %>%
+      rename(city = "GEO_NAME",
+             sofac = "CHARACTERISTIC_ID",
+             sonum = "C1_COUNT_TOTAL") %>%
+      filter(sofac %in% c(1, 6:7, 42:49, 115, 331, 1522, 1389, 1670)) # missing education right now
   ),
   
   tar_target(
-    census_w,
-    census_f %>% pivot_wider(names_from = sofac, values_from = sonum)
+    census_cma_f,
+    census_cma_raw %>%
+      filter("GEO_NAME" == "Vancouver" |
+               "GEO_NAME" == "Calgary" | 
+               "GEO_NAME" == "Winnipeg" | 
+               "GEO_NAME" == "Toronto" |
+               "GEO_NAME" == "Ottawa - Gatineau (Ontario part)" |
+               "GEO_NAME" == "Montréal" | 
+               "GEO_NAME" == "Halifax") %>% 
+      mutate(GEO_NAME = replace(GEO_NAME, GEO_NAME == "Ottawa - Gatineau (Ontario part / partie de l'Ontario)", "Ottawa")) %>%
+      select("GEO_NAME", "CHARACTERISTIC_ID", "C1_COUNT_TOTAL") %>% 
+      rename(city = "GEO_NAME",
+             sofac = "CHARACTERISTIC_ID",
+             sonum = "C1_COUNT_TOTAL") %>%
+      filter(sofac %in% c(1, 6:7, 42:49, 115, 331, 1522, 1389, 1670)) # missing education right now
+  ),
+  
+  # 1 = Population 2021
+  # 6 = Pop density per sq km
+  # 7 = Land area sq km
+  # 41 = Total - Occupied private dwellings by structural type of dwelling - 100% data (7)
+  # 42 = Single-detached house
+  # 43 = Semi-detached house
+  # 44 = Row house
+  # 45 = Apartment or flat in a duplex
+  # 46 = Apartment in a building that has fewer than five storeys
+  # 47 = Apartment in a building that has five or more storeys
+  # 48 = Other single-attached house
+  # 49 = Movable dwelling (9)
+  # 115 = Median after-tax income in 2015 among recipients 
+  # 331 = Prevalence of low income based on the Low-income measure, after tax (LIM-AT) (%)
+  # 1522 = Total - Immigrant status and period of immigration for the population in private households - 25% sample data (63) -> Immigrants -> 2011-2016 (recent immigrants)
+  # 1389 = Total - Aboriginal identity for the population in private households - 25% sample data (104) -> Aboriginal identity (105)
+  # 1670 = Total - Visible minority for the population in private households - 25% sample data (121) -> Total visible minority population (122)
+  # we want percentages for all datasets except median income - need to transform 41:50, 1149, 1290, 1324, 1692
+  # start with dwelling types
+
+  
+  tar_target(
+    census_da_w,
+    census_da_f %>% pivot_wider(names_from = sofac, values_from = sonum)
   ),
   
   tar_target(
-    census_r,
-    census_w %>%
+    census_cma_w, 
+    census_cma_f %>% pivot_wider(names_from = sofac, values_from = sonum)
+  ),
+  
+  tar_target(
+    census_da_r,
+    census_da_w %>%
       rename(totpop = "1") %>%
       rename(popdens = "6") %>%
       rename(area = "7") %>%
       rename(sideho = "42") %>%
-      rename(aptfiv = "43") %>%
-      rename(oadwel = "44") %>%
-      rename(semhou = "45") %>%
-      rename(rowhou = "46") %>%
-      rename(aptdup = "47") %>%
-      rename(aptbui = "48") %>%
-      rename(otsiho = "49") %>%
-      rename(mvdwel = "50") %>%
-      rename(medinc = "665") %>%
-      rename(lowinc = "857") %>%
-      rename(recimm = "1149") %>%
-      rename(aborig = "1290") %>%
-      rename(vismin = "1324") %>%
-      rename(edubac = "1692")
+      rename(semhou = "43") %>%
+      rename(rowhou = "44") %>%
+      rename(aptdup = "45") %>%
+      rename(aptbui = "46") %>%
+      rename(aptfiv = "47") %>%
+      rename(otsiho = "48") %>%
+      rename(mvdwel = "49") %>%
+      rename(medinc = "115") %>%
+      rename(lowinc = "331") %>%
+      rename(recimm = "1522") %>%
+      rename(aborig = "1389") %>%
+      rename(vismin = "1670") 
+    #rename(edubac = "1692")
   ),
   
   tar_target(
-    census_DA_m,
-    merge(census_r, DA_bound, by = "dsa")
+    census_cma_r,
+    census_cma_w %>%
+      rename(totpop = "1") %>%
+      rename(popdens = "6") %>%
+      rename(area = "7") %>%
+      rename(sideho = "42") %>%
+      rename(semhou = "43") %>%
+      rename(rowhou = "44") %>%
+      rename(aptdup = "45") %>%
+      rename(aptbui = "46") %>%
+      rename(aptfiv = "47") %>%
+      rename(otsiho = "48") %>%
+      rename(mvdwel = "49") %>%
+      rename(medinc = "115") %>%
+      rename(lowinc = "331") %>%
+      rename(recimm = "1522") %>%
+      rename(aborig = "1389") %>%
+      rename(vismin = "1670") 
+      #rename(edubac = "1692")
   ),
   
   tar_target(
-    census_DA_sf,
-    st_as_sf(census_DA_m, sf_column_name = c("geometry"), crs = 3347)
+    census_da_m,
+    merge(census_da_r, da_bound, by = "dsa")
   ),
   
   tar_target(
-    census_DA_na,
-    census_DA_sf %>% mutate(across(c(2:20), ~na_if(., "x")),
-                            across(c(2:20), ~na_if(., "F")))
+    census_cma_m,
+    mun_bound %>%
+      rename(city = CMANAME) %>%
+      merge(., census_cma_r, by = "city")
   ),
   
   tar_target(
-    census_DA_num, 
-    census_DA_na %>% 
+    census_da_sf,
+    st_as_sf(census_da_m, sf_column_name = c("geometry"), crs = 3347)
+  ),
+  
+  tar_target(
+    census_cma_sf,
+    st_as_sf(census_cma_m, sf_column_name = c("geometry"), crs = 3347)
+  ),
+  
+  tar_target(
+    census_da_na,
+    census_da_sf %>% mutate(across(c(2:18), ~na_if(., "x")),
+                            across(c(2:18), ~na_if(., "F")))
+  ),
+  
+  tar_target(
+    census_cma_na,
+    census_cma_sf %>% mutate(across(c(2:18), ~na_if(., "x")),
+                            across(c(2:18), ~na_if(., "F")))
+  ),
+  
+  tar_target(
+    census_da_num, 
+    census_da_na %>% 
       mutate(dsa = as.factor(dsa)) %>%
-      mutate(across(c(2:19), ~as.numeric(.))) %>%
+      mutate(across(c(2:18), ~as.numeric(.))) %>%
       drop_na()
   ),
   
-  # 41 = Total - Occupied private dwellings by structural type of dwelling - 100% data (7)
-  # 42 = Single-detached house
-  # 43 = Apartment in a building that has five or more storeys
-  # 44 = Other attached dwelling (8)
-  # 45 = Semi-detached house
-  # 46 = Row house
-  # 47 = Apartment or flat in a duplex
-  # 48 = Apartment in a building that has fewer than five storeys
-  # 49 = Other single-attached house
-  # 50 = Movable dwelling (9)
-  # 665 = Median after-tax income in 2015 among recipients 
-  # 857 = Prevalence of low income based on the Low-income measure, after tax (LIM-AT) (%)
-  # 1149 = Total - Immigrant status and period of immigration for the population in private households - 25% sample data (63) -> Immigrants -> 2011-2016 (recent immigrants)
-  # 1290 = Total - Aboriginal identity for the population in private households - 25% sample data (104) -> Aboriginal identity (105)
-  # 1324 = Total - Visible minority for the population in private households - 25% sample data (121) -> Total visible minority population (122)
-  # 1692 = Total - Highest certificate, diploma or degree for the population aged 15 years and over in private households - 25% sample data (178) -> University certificate, diploma or degree at bachelor level or above
-  # we want percentages for all datasets except median income - need to transform 41:50, 1149, 1290, 1324, 1692
-  # start with dwelling types
-  
   tar_target(
-    census_dt, 
-    setDT(census_DA_num)
+    census_cma_num, 
+    census_cma_na %>% 
+      mutate(city = as.factor(city)) %>%
+      mutate(across(c(2:18), ~as.numeric(.))) %>%
+      drop_na()
   ),
   
   tar_target(
-    census,
-    calculate_percent(census_dt)
+    census_da_dt, 
+    setDT(census_da_num)
+  ),
+  
+  
+  tar_target(
+    census_cma_dt, 
+    setDT(census_cma_num)
   )
+  
+#  tar_target(
+#    census,
+#    calculate_percent(census_dt)
+#  )
+#  
+#  ## ADD FINAL STEP
   
 )
