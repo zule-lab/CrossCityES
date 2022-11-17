859bb712228f890bf873b0c46b5aa5d12240b382
icrichmond <icrichmond@mun.ca>
2022-11-17 13:20:33 -0500

    call rasters with quotes

--- scripts/height-data-city-neighbourhood-road.R
+++ scripts/height-data-city-neighbourhood-road.R
@@ -2,107 +2,107 @@ height_data_neighbourhood_road <- c(
   
   tar_target(
     van_city_height, 
-    height_city(van_build_height, vancouver.tif)
+    height_city(van_build_height, 'vancouver.tif')
   ),
   
   tar_target(
     van_hood_height,
-    height_hood(van_hood, van_build_height, vancouver.tif)
+    height_hood(van_hood, van_build_height, 'vancouver.tif')
   ),
   
   tar_target(
     van_road_height,
-    height_road(mun_road, van_build_height, tifname)
+    height_road(mun_road, van_build_height, 'vancouver.tif')
   ),
   
   tar_target(
     cal_city_height, 
-    height_city(cal_build_height, calgarywest.tif)
+    height_city(cal_build_height, 'calgarywest.tif')
   ),
   
   tar_target(
     cal_hood_height,
-    height_hood(cal_hood, cal_build_height, calgarywest.tif)
+    height_hood(cal_hood, cal_build_height, 'calgarywest.tif')
   ),
   
   tar_target(
     cal_road_height,
-    height_road(mun_road, cal_build_height, calgarywest.tif)
+    height_road(mun_road, cal_build_height, 'calgarywest.tif')
   ),
   
   tar_target(
     win_city_height, 
-    height_city(win_build_height, winnipegne.tif)
+    height_city(win_build_height, 'winnipegne.tif')
   ),
   
   tar_target(
     win_hood_height,
-    height_hood(win_hood, win_build_height, winnipegne.tif)
+    height_hood(win_hood, win_build_height, 'winnipegne.tif')
   ),
   
   tar_target(
     win_road_height,
-    height_road(mun_road, win_build_height, winnipegne.tif)
+    height_road(mun_road, win_build_height, 'winnipegne.tif')
   ),
   
   tar_target(
     tor_city_height,
-    height_city(tor_build_height, torontone.tif)
+    height_city(tor_build_height, 'torontone.tif')
   ),
   
   tar_target(
     tor_hood_height,
-    height_hood(tor_hood, tor_build_height, torontone.tif)
+    height_hood(tor_hood, tor_build_height, 'torontone.tif')
   ),
   
   tar_target(
     tor_road_height,
-    height_road(mun_road, tor_build_height, torontone.tif)
+    height_road(mun_road, tor_build_height, 'torontone.tif')
   ),
   
   tar_target(
     ott_city_height,
-    height_city(ott_build_height, ottawane.tif)
+    height_city(ott_build_height, 'ottawane.tif')
   ),
   
   tar_target(
     ott_hood_height,
-    height_hood(ott_hood, ott_build_height, ottawane.tif)
+    height_hood(ott_hood, ott_build_height, 'ottawane.tif')
   ),
   
   tar_target(
     ott_road_height,
-    height_road(mun_road, ott_build_height, ottawane.tif)
+    height_road(mun_road, ott_build_height, 'ottawane.tif')
   ),
   
   tar_target(
     mon_city_height,
-    height_city(mon_build_height, cotesaintluc.tif)
+    height_city(mon_build_height, 'cotesaintluc.tif')
   ),
-  
+
   tar_target(
     mon_hood_height,
-    height_hood(mon_hood, mon_build_height, cotesaintluc.tif)
+    height_hood(mon_hood, mon_build_height, 'cotesaintluc.tif')
   ),
   
   tar_target(
     mon_road_height,
-    height_road(mun_road, mon_build_height, cotesaintluc.tif)
+    height_road(mun_road, mon_build_height, 'cotesaintluc.tif')
   ),
   
   tar_target(
     hal_city_height, 
-    height_city(hal_build_height, halifax.tif)
+    height_city(hal_build_height, 'halifax.tif')
   ),
   
   tar_target(
     hal_hood_height,
-    height_hood(hal_hood, hal_build_height, halifax.tif)
+    height_hood(hal_hood, hal_build_height, 'halifax.tif')
   ),
   
   tar_target(
     hal_road_height,
-    height_road(mun_road, hal_build_height, halifax.tif)
+    height_road(mun_road, hal_build_height, 'halifax.tif')
   ),
   
   tar_target(
