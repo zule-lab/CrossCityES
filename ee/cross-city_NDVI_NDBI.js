84112ceb730fa6c7e522d94b3af10397e8feb328
icrichmond <icrichmond@mun.ca>
2022-11-17 12:46:21 -0500

    WORKING NDVI

--- ee/cross-city_NDVI_NDBI.js
+++ ee/cross-city_NDVI_NDBI.js
@@ -1,12 +1,6 @@
 // Data Import + Cloud Removal -----------------------------------
 // code from this tutorial: https://developers.google.com/earth-engine/tutorials/community/sentinel-2-s2cloudless
 
-// Sentinel-2 Level 1C data.  Bands B7, B8, B8A and B10 from this
-// dataset are needed as input to CDI and the cloud mask function.
-var s2 = ee.ImageCollection('COPERNICUS/S2');
-// Cloud probability dataset.  The probability band is used in
-// the cloud mask function.
-var s2c = ee.ImageCollection('COPERNICUS/S2_CLOUD_PROBABILITY');
 // Sentinel-2 surface reflectance data for the composite.
 var s2Sr = ee.ImageCollection('COPERNICUS/S2_SR');
 
@@ -18,70 +12,12 @@ Map.centerObject(cities, 11);
 var start = ee.Date('2021-06-01');
 var end = ee.Date('2021-08-30');
 
-// S2 L1C for Cloud Displacement Index (CDI) bands.
-s2 = s2.filterBounds(roi).filterDate(start, end)
-    .select(['B7', 'B8', 'B8A', 'B10']);
-// S2Cloudless for the cloud probability band.
-s2c = s2c.filterDate(start, end).filterBounds(roi);
 // S2 L2A for surface reflectance bands.
-s2Sr = s2Sr.filterDate(start, end).filterBounds(roi)
-    .select(['B2', 'B3', 'B4', 'B5', 'B8', 'B11']);
-
-// Join two collections on their 'system:index' property.
-// The propertyName parameter is the name of the property
-// that references the joined image.
-function indexJoin(collectionA, collectionB, propertyName) {
-  var joined = ee.ImageCollection(ee.Join.saveFirst(propertyName).apply({
-    primary: collectionA,
-    secondary: collectionB,
-    condition: ee.Filter.equals({
-      leftField: 'system:index',
-      rightField: 'system:index'})
-  }));
-  // Merge the bands of the joined image.
-  return joined.map(function(image) {
-    return image.addBands(ee.Image(image.get(propertyName)));
-  });
-}
-
-// Aggressively mask clouds and shadows.
-function maskImage(image) {
-  // Compute the cloud displacement index from the L1C bands.
-  var cdi = ee.Algorithms.Sentinel2.CDI(image);
-  var s2c = image.select('probability');
-  var cirrus = image.select('B10').multiply(0.0001);
-
-  // Assume low-to-mid atmospheric clouds to be pixels where probability
-  // is greater than 65%, and CDI is less than -0.5. For higher atmosphere
-  // cirrus clouds, assume the cirrus band is greater than 0.01.
-  // The final cloud mask is one or both of these conditions.
-  var isCloud = s2c.gt(65).and(cdi.lt(-0.5)).or(cirrus.gt(0.01));
-
-  // Reproject is required to perform spatial operations at 20m scale.
-  // IR NOTE: changed scale from 20 m as in tutorial to 10 m so that pixels can be used to calculate NDVI at 10 m scale
-  isCloud = isCloud.focal_min(3).focal_max(16);
-  isCloud = isCloud.reproject({crs: cdi.projection(), scale: 10});
-
-  // Project shadows from clouds we found in the last step. This assumes we're working in
-  // a UTM projection.
-  var shadowAzimuth = ee.Number(90)
-      .subtract(ee.Number(image.get('MEAN_SOLAR_AZIMUTH_ANGLE')));
-
-  // With the following reproject, the shadows are projected 5km.
-  isCloud = isCloud.directionalDistanceTransform(shadowAzimuth, 50);
-  isCloud = isCloud.reproject({crs: cdi.projection(), scale: 100});
-
-  isCloud = isCloud.select('distance').mask();
-  return image.select('B2', 'B3', 'B4', 'B8', 'B11').updateMask(isCloud.not());
-}
-
-// Join the cloud probability dataset to surface reflectance.
-var withCloudProbability = indexJoin(s2Sr, s2c, 'cloud_probability');
-// Join the L1C data to get the bands needed for CDI.
-var withS2L1C = indexJoin(withCloudProbability, s2, 'l1c');
-
-// Map the cloud masking function over the joined collection.
-var masked = ee.ImageCollection(withS2L1C.map(maskImage));
+s2Sr = s2Sr
+.filterDate(start, end)
+.filterBounds(roi)
+.select(['B2', 'B3', 'B4', 'B5', 'B8', 'B11'])
+.filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10));
 
 
 // Calculating NDVI & NDBI ------------------------------------------------------------------------------
@@ -96,11 +32,11 @@ var addNDBI = function(image) {
 };
 
 
-var masked_NDVI = masked.map(addNDVI);
-var masked_NDVI_NDBI = masked_NDVI.map(addNDBI);
+var s2Sr_NDVI = s2Sr.map(addNDVI);
+var s2Sr_NDVI_NDBI = s2Sr_NDVI.map(addNDBI);
 
-var indices = masked_NDVI_NDBI.select(['NDVI', 'NDBI'])
-var indicesMean = indices.select(['NDVI', 'NDBI']).mean()
+var indices = s2Sr_NDVI_NDBI.select(['NDVI', 'NDBI'])
+var indicesMean = indices.mean()
 
 
 // Reducing to Regions ------------------------------------------------------------------------------------
@@ -113,9 +49,10 @@ var reducer = ee.Reducer.mean()
 
 
 var NDVI_NDBI_city = indicesMean.reduceRegions({
+  'collection': cities,
   'reducer': reducer,
-  'scale': 10, 
-  'collection': cities});
+  'scale': 10 
+});
 
 var NDVI_NDBI_hood = indicesMean.reduceRegions({
   'reducer': reducer,
@@ -126,7 +63,6 @@ var NDVI_NDBI_roads = indicesMean.reduceRegions({
   'reducer': reducer,
   'scale': 10, 
   'collection': roads});
-
 
 
 // Export --------------------------------------------------------------------------------------------------
@@ -152,4 +88,4 @@ Export.table.toDrive({
 
 // Visualize ----------------------------------------------------------------------------------------------
 var NDVIpalette = ['FFFFFF', 'CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718', '74A901', '66A000', '529400', '3E8601', '207401', '056201', '004C00', '023B01', '012E01', '011D01', '011301'];
-Map.addLayer(NDVI_NDBI_city.select('NDVI_mean'));
+Map.addLayer(NDVI_NDBI_city.select('NDVI_mean'), NDVIpalette, 'NDVI');
