34ce440078d0ea06cbe191bd27a4fd6898fb0513
icrichmond <icrichmond@mun.ca>
2022-11-22 11:40:02 -0500

    setting units

--- scripts/treesize-data-city-neighbourhood-road.R
+++ scripts/treesize-data-city-neighbourhood-road.R
@@ -15,7 +15,11 @@ treesize_data_city_neighbourhood_road <- c(
       summarize(mean_ba = mean(basal_area),
                 sd_ba = sd(basal_area),
                 mean_dbh = mean(dbh_in),
-                sd_dbh = mean(dbh_in)) # NOTE: in square feet
+                sd_dbh = mean(dbh_in)) %>%
+      mutate(mean_ba = set_units(mean_ba, "ft2"),
+             sd_ba = set_units(sd_ba, "ft2"),
+             mean_dbh = set_units(mean_dbh, "in"),
+             sd_dbh = set_units(sd_dbh, "in"))
     
   ),
   
@@ -26,7 +30,11 @@ treesize_data_city_neighbourhood_road <- c(
       summarize(mean_ba = mean(basal_area), # NOTE: in square feet
                 sd_ba = sd(basal_area),
                 mean_dbh = mean(dbh_in),
-                sd_dbh = sd(dbh_in)) 
+                sd_dbh = sd(dbh_in)) %>%
+      mutate(mean_ba = set_units(mean_ba, "ft2"),
+             sd_ba = set_units(sd_ba, "ft2"),
+             mean_dbh = set_units(mean_dbh, "in"),
+             sd_dbh = set_units(sd_dbh, "in"))
     
   ),
   
@@ -38,7 +46,11 @@ treesize_data_city_neighbourhood_road <- c(
       summarize(mean_ba = mean(basal_area), # NOTE: in square feet
                 sd_ba = sd(basal_area),
                 mean_dbh = mean(dbh_in),
-                sd_dbh = sd(dbh_in)) 
+                sd_dbh = sd(dbh_in)) %>%
+      mutate(mean_ba = set_units(mean_ba, "ft2"),
+             sd_ba = set_units(sd_ba, "ft2"),
+             mean_dbh = set_units(mean_dbh, "in"),
+             sd_dbh = set_units(sd_dbh, "in"))
     
   ),
   
