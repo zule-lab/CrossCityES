2d3b3fb59b55c947056b601533d64971385c616a
icrichmond <icrichmond@mun.ca>
2022-11-22 10:55:04 -0500

    matrix formatting (still wrong)

--- scripts/richness-data-city-neighbourhood-road.R
+++ scripts/richness-data-city-neighbourhood-road.R
@@ -8,22 +8,31 @@ richness_data_city_neighbourhood_road <- c(
       mutate(n = n()) %>%
       select(c(city, fullname, n)) %>% 
       st_set_geometry(NULL) %>%
-      pivot_wider(names_from = 'fullname', values_from = 'n', values_fill = 0, values_fn = first) %>%
-      mutate_if(is.numeric, ~1 * (. != 0)) %>%
-      column_to_rownames("city")
+      group_map(~ pivot_wider(data = .x, names_from = 'fullname', values_from = 'n', values_fill = 0, values_fn = first) %>%
+                  mutate_if(is.numeric, ~1 * (. != 0))) %>% 
+      setNames(unique(all_tree$city))
   ),
   
   tar_target(
    hood_matrix, 
-    all_tree %>% 
-      drop_na(species) %>%
-      group_by(hood_id) %>% 
-      mutate(n = n()) %>%
-      select(c(hood_id, fullname, n)) %>% 
-      st_set_geometry(NULL) %>%
-      pivot_wider(names_from = 'fullname', values_from = 'n', values_fill = 0, values_fn = first) %>%
-      mutate_if(is.numeric, ~1 * (. != 0)) %>%
-      column_to_rownames("hood_id")
+   all_tree %>% 
+     group_by(hood_id) %>% 
+     mutate(n = n()) %>%
+     select(c(hood_id, fullname, n)) %>% 
+     st_set_geometry(NULL) %>%
+     group_map(~ pivot_wider(data = .x, names_from = 'fullname', values_from = 'n', values_fill = 0, values_fn = first) %>%
+                 mutate_if(is.numeric, ~1 * (. != 0))) %>% 
+     setNames(unique(all_tree$hood_id))
+  ),
+  
+  tar_target(
+    city_inext, 
+    iNEXT(city_matrix, datatype = "incidence_raw", q = 0)
+  ),
+  
+  tar_target(
+    hood_inext,
+    iNEXT(hood_matrix, datatype = "incidence_raw", q = 0)
   )
   
   
