45f39dde5f4b23bd41ef4683c2c4c631cc4350b6
icrichmond <icrichmond@mun.ca>
2022-11-23 13:24:43 -0500

    add city name column

--- R/height_city.R
+++ R/height_city.R
@@ -1,9 +1,11 @@
-height_city <- function(city_build_height, tifname){
+height_city <- function(city_build_height, tifname, cityname){
   city_build_height %>%
     rename(height = tifname) %>%
+    mutate(city = cityname) %>%
     st_set_geometry(NULL) %>%
     filter(height > 0) %>% 
-    summarize(mean = mean(height),
+    summarize(city = cityname, 
+              mean = mean(height),
               median = median(height),
               sd = sd(height))
 }
