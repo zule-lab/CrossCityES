0e1658c47c6bc2c5e4384c67890a22023d0c4495
icrichmond <icrichmond@mun.ca>
2022-11-23 11:23:33 -0500

    add labels to dfs

--- R/clean_sat.R
+++ R/clean_sat.R
@@ -1,13 +1,8 @@
-clean_sat <- function(df, ...){
+clean_sat <- function(df, label){
 
   # remove system:index and geo columns because they are not in useable format
-  df <- df %>% select(-c('system.index', '.geo'))
-  
-  if(missing(...)) {
-    df <- df
-  } else {
-    df %>% rename_with(~paste0(.x, ...), all_of(c("mean", "median", "max", "min", "count", "stdDev")))
-  }
+  df <- df %>% select(-c('system:index', '.geo')) %>% 
+    rename_with(., ~ paste0(.x, "_", label), .cols = all_of(c("mean", "median", "max", "min", "count", "stdDev")))
   
   return(df)
   
--- scripts/pollution-data-cleanup.R
+++ scripts/pollution-data-cleanup.R
@@ -74,51 +74,51 @@ pollution_data_cleanup <- c(
 
   tar_target(
     CO_city,
-    clean_sat(CO_city_raw)
+    clean_sat(CO_city_raw, "CO")
   ),
 
   tar_target(
     CO_hood,
-    clean_sat(CO_hood_raw)
+    clean_sat(CO_hood_raw, "CO")
   ),
 
   tar_target(
     NO2_city,
-    clean_sat(NO2_city_raw)
+    clean_sat(NO2_city_raw, "NO2")
   ),
   
   tar_target(
     NO2_hood,
-    clean_sat(NO2_hood_raw)
+    clean_sat(NO2_hood_raw, "NO2")
   ),
 
   tar_target(
     O3_city,
-    clean_sat(O3_city_raw)
+    clean_sat(O3_city_raw, "O3")
   ),
   
   tar_target(
     O3_hood,
-    clean_sat(O3_hood_raw)
+    clean_sat(O3_hood_raw, "O3")
   ),
 
   tar_target(
     SO2_city,
-    clean_sat(SO2_city_raw)
+    clean_sat(SO2_city_raw, "SO2")
   ),
   
   tar_target(
     SO2_hood,
-    clean_sat(SO2_hood_raw)
+    clean_sat(SO2_hood_raw, "SO2")
   ),
 
   tar_target(
     UV_city,
-    clean_sat(UV_city_raw)
+    clean_sat(UV_city_raw, "UV")
   ),
   
   tar_target(
     UV_hood,
-    clean_sat(UV_hood_raw)
+    clean_sat(UV_hood_raw, "UV")
   )
 )
