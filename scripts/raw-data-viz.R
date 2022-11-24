91a4079e454969c32d68df36c95fe99200509f1c
icrichmond <icrichmond@mun.ca>
2022-11-24 13:59:00 -0500

    typo for neighbourhood dataset

--- scripts/raw-data-viz.R
+++ scripts/raw-data-viz.R
@@ -10,12 +10,12 @@ raw_data_viz <- c(
       inner_join(., ndi_city, by = "city") %>%
       inner_join(., city_height, by = "city", suffix = c("", "_buildhgt")) %>%
       inner_join(., city_road_prop, by = "city") %>%
-      left_join(., census_cma, by = "city") # missing Montreal - encoding issue  
+      inner_join(., census_cma, by = "city") # missing Montreal - encoding issue  
   ),
   
   tar_target(
     neighbourhood_data, 
-    temp_city %>%
+    temp_hood %>%
       inner_join(., pollution_hood, by = "hood_id") %>%
       inner_join(., treesize_hood, by = "hood_id") %>% 
       inner_join(., treedensity_hood, by = "hood_id") %>%
