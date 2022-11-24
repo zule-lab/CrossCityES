aa6d6ac1d1d343bf9fb71e38673704c26ed33d6f
icrichmond <icrichmond@mun.ca>
2022-11-24 17:06:18 -0500

    add pca plots

--- scripts/raw-data-viz.R
+++ scripts/raw-data-viz.R
@@ -21,6 +21,26 @@ raw_data_viz <- c(
   ),
   
   tar_target(
+    city_response_pca,
+    city_pca(city_response_vars)
+  ),
+  
+  tar_target(
+    city_tree_pca, 
+    city_pca(city_tree_vars)
+  ),
+  
+  tar_target(
+    city_build_pca, 
+    city_pca(city_build_vars)
+  ),
+  
+  tar_target(
+    city_census_pca, 
+    city_pca(city_census_vars)
+  ),
+  
+  tar_target(
     neighbourhood_response_facet, 
     neighbourhood_plot(neighbourhood_response_vars)
   ),
@@ -38,6 +58,26 @@ raw_data_viz <- c(
   tar_target(
     neighbourhood_census_facet,
     neighbourhood_plot(neighbourhood_census_vars)
+  ),
+  
+  tar_target(
+    neighbourhood_response_pca, 
+    neighbourhood_pca(neighbourhood_response_vars)
+  ),
+  
+  tar_target(
+    neighbourhood_tree_pca, 
+    neighbourhood_pca(neighbourhood_tree_vars)
+  ),
+  
+  tar_target(
+    neighbourhood_build_pca, 
+    neighbourhood_pca(neighbourhood_build_vars)
+  ),
+  
+  tar_target(
+    neighbourhood_census_pca,
+    neighbourhood_pca(neighbourhood_census_vars)
   )
   
     
