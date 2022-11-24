c4e1951b0613fddd1337178aff2b2a6843a47582
icrichmond <icrichmond@mun.ca>
2022-11-24 17:06:35 -0500

    plot pcas functions

---
+++ R/city_pca.R
@@ -0,0 +1,21 @@
+city_pca <- function(x){
+  
+  x <- x %>% mutate_at(vars(-("city")), as.numeric)
+  
+  #PCA
+  pca <- PCA(x[,-c(1)], scale.unit=TRUE, ncp=14, graph=T)
+  
+  
+  #both points and FA axes
+  pca_plot <- fviz_pca_biplot(pca, label = "var", col.var = "gray22", col.ind = x$city, pointsize = 3,
+                  addEllipses = TRUE, ellipse.level = 0.4, invisible = "quali", axes.linetype = "solid") +
+    scale_color_manual(values=met.brewer("Veronese", 7)) + 
+    scale_fill_manual(values=met.brewer("Veronese", 7)) +
+    scale_shape_manual(values=c(17, 17, 17, 17, 17, 17, 17), name = "city", guide = "none") + 
+    theme_minimal() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    labs(title = "")
+  
+  return(pca_plot)
+  
+}
