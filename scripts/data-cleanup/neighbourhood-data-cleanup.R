78a57399a65d53fe22cb5f37ec9e115ac3308f01
icrichmond <icrichmond@mun.ca>
2022-12-07 16:23:36 -0500

    uncomment

--- scripts/data-cleanup/neighbourhood-data-cleanup.R
+++ scripts/data-cleanup/neighbourhood-data-cleanup.R
@@ -6,23 +6,21 @@ targets_neighbourhood_cleanup <- c(
       cleaned_neighbourhoods(tar_load(file_name))
     ),
     values = filter(values_trib, grepl('hood_raw', file_name))
-  )
+  ),
   
-#  
-#  tar_target(
-#    can_hood,
-#    rbind(cal_hood, hal_hood, mon_hood, ott_hood, tor_hood, van_hood, win_hood)
-#  ),
-#  
-#  tar_target(
-#    can_hood_ee,
-#    can_hood %>%
-#      sf_as_ee(x = .,
-#             overwrite = TRUE,
-#             assetId = sprintf("%s/%s", ee_get_assethome(), 'mun_road'),
-#             bucket = 'rgee_dev',
-#             monitoring = FALSE,
-#             via = 'gcs_to_asset')
-#  )
-
+  tar_target(
+    can_hood,
+    rbind(clean_van_hood, clean_cal_hood, clean_win_hood, clean_tor_hood, clean_ott_hood, clean_hal_hood)
+  ),
+  
+  tar_target(
+    can_hood_ee,
+    can_hood %>%
+      sf_as_ee(x = .,
+             overwrite = TRUE,
+             assetId = sprintf("%s/%s", ee_get_assethome(), 'mun_road'),
+             bucket = 'rgee_dev',
+             monitoring = FALSE,
+             via = 'gcs_to_asset')
+  )
 )
