a41b10a6693de231ae2a9fc849602696b57e0946
icrichmond <icrichmond@mun.ca>
2022-12-07 11:17:22 -0500

    repetitive targets to tar_eval

--- scripts/data-cleanup/neighbourhood-data-cleanup.R
+++ scripts/data-cleanup/neighbourhood-data-cleanup.R
@@ -1,73 +1,6 @@
 targets_neighbourhood_cleanup <- c(
   
   tar_target(
-    cal_hood_i,
-    cal_hood_raw %>% 
-      select(c("NAME", "MULTIPOLYGON")) %>% 
-      rename("hood" = "NAME") %>% 
-      rename("geometry" = "MULTIPOLYGON") %>%
-      mutate(city = c("Calgary")) %>%
-      st_as_sf(wkt = "geometry", crs = 4326)
-  ),
-  
-  tar_target(
-    cal_hood,
-    hood_cleaned(cal_hood_i, "cal")
-  ),
-  
-  tar_target(
-    hal_hood_i,
-    hal_hood_raw %>% 
-      select(c("GSA_NAME", "geometry")) %>% 
-      rename("hood" = "GSA_NAME") %>% 
-      mutate(city = c("Halifax")) 
-  ),
-  
-  tar_target(
-    hal_hood,
-    hood_cleaned(hal_hood_i, "hal")
-  ),
-  
-  tar_target(
-    mon_hood_i, 
-    mon_hood_raw %>% 
-      select(c("NOM","geometry")) %>%
-      rename("hood" = "NOM") %>%
-      mutate(city = c("Montreal"))
-  ),
-  
-  tar_target(
-    mon_hood,
-    hood_cleaned(mon_hood_i, "mon")
-  ),
-  
-  tar_target(
-    ott_hood_i,
-    ott_hood_raw %>% 
-      select(c("Name", "geometry")) %>% 
-      rename("hood" = "Name") %>%
-      mutate(city = c("Ottawa"))
-  ),
-  
-  tar_target(
-    ott_hood,
-    hood_cleaned(ott_hood_i, "ott")
-  ),
-  
-  tar_target(
-    tor_hood_i,
-    tor_hood_raw %>% 
-      select(c("FIELD_8", "geometry")) %>% 
-      rename("hood" = "FIELD_8")%>%
-      mutate(city = c("Toronto"))
-  ),
-  
-  tar_target(
-    tor_hood,
-    hood_cleaned(tor_hood_i, "tor")
-  ),
-  
-  tar_target(
     van_hood_i,
     van_hood_raw %>% 
       select(c("name","geometry"))%>%
@@ -76,8 +9,13 @@ targets_neighbourhood_cleanup <- c(
   ),
   
   tar_target(
-    van_hood,
-    hood_cleaned(van_hood_i, "van")
+    cal_hood_i,
+    cal_hood_raw %>% 
+      select(c("NAME", "MULTIPOLYGON")) %>% 
+      rename("hood" = "NAME") %>% 
+      rename("geometry" = "MULTIPOLYGON") %>%
+      mutate(city = c("Calgary")) %>%
+      st_as_sf(wkt = "geometry", crs = 4326)
   ),
   
   tar_target(
@@ -89,8 +27,43 @@ targets_neighbourhood_cleanup <- c(
   ),
   
   tar_target(
-    win_hood,
-    hood_cleaned(win_hood_i, "win")
+    tor_hood_i,
+    tor_hood_raw %>% 
+      select(c("FIELD_8", "geometry")) %>% 
+      rename("hood" = "FIELD_8")%>%
+      mutate(city = c("Toronto"))
+  ),
+  
+  tar_target(
+    ott_hood_i,
+    ott_hood_raw %>% 
+      select(c("Name", "geometry")) %>% 
+      rename("hood" = "Name") %>%
+      mutate(city = c("Ottawa"))
+  ),
+  
+  tar_target(
+    mon_hood_i, 
+    mon_hood_raw %>% 
+      select(c("NOM","geometry")) %>%
+      rename("hood" = "NOM") %>%
+      mutate(city = c("Montreal"))
+  ),
+  
+  tar_target(
+    hal_hood_i,
+    hal_hood_raw %>% 
+      select(c("GSA_NAME", "geometry")) %>% 
+      rename("hood" = "GSA_NAME") %>% 
+      mutate(city = c("Halifax")) 
+  ),
+  
+  tar_eval(
+    tar_target(
+      name,
+      hood_cleaned(tar, substr(name, 1, 3))),
+    values = list(name = lapply(c('van_hood', 'cal_hood', 'win_hood', 'tor_hood', 'ott_hood', 'mon_hood', 'hal_hood'), as.symbol),
+                  file = list(van_hood_i, cal_hood_i, win_hood_i, tor_hood_i, ott_hood_i, mon_hood_i, hal_hood_i))
   ),
   
   tar_target(
