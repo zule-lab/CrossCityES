2e20b4f9482ddf682ac16e9c76f32967899df4cd
icrichmond <icrichmond@mun.ca>
2022-12-07 15:45:45 -0500

    attempting to switch to tar_eval - not working

--- scripts/data-cleanup/neighbourhood-data-cleanup.R
+++ scripts/data-cleanup/neighbourhood-data-cleanup.R
@@ -1,85 +1,28 @@
 targets_neighbourhood_cleanup <- c(
   
-  tar_target(
-    van_hood_i,
-    van_hood_raw %>% 
-      select(c("name","geometry"))%>%
-      rename(hood = "name") %>%
-      mutate(city = c("Vancouver"))
-  ),
-  
-  tar_target(
-    cal_hood_i,
-    cal_hood_raw %>% 
-      select(c("NAME", "MULTIPOLYGON")) %>% 
-      rename("hood" = "NAME") %>% 
-      rename("geometry" = "MULTIPOLYGON") %>%
-      mutate(city = c("Calgary")) %>%
-      st_as_sf(wkt = "geometry", crs = 4326)
-  ),
-  
-  tar_target(
-    win_hood_i,
-    win_hood_raw %>% 
-      select(c("name","geometry")) %>%
-      rename(hood = "name") %>%
-      mutate(city = c("Winnipeg"))
-  ),
-  
-  tar_target(
-    tor_hood_i,
-    tor_hood_raw %>% 
-      select(c("FIELD_8", "geometry")) %>% 
-      rename("hood" = "FIELD_8")%>%
-      mutate(city = c("Toronto"))
-  ),
-  
-  tar_target(
-    ott_hood_i,
-    ott_hood_raw %>% 
-      select(c("Name", "geometry")) %>% 
-      rename("hood" = "Name") %>%
-      mutate(city = c("Ottawa"))
-  ),
-  
-  tar_target(
-    mon_hood_i, 
-    mon_hood_raw %>% 
-      select(c("NOM","geometry")) %>%
-      rename("hood" = "NOM") %>%
-      mutate(city = c("Montreal"))
-  ),
-  
-  tar_target(
-    hal_hood_i,
-    hal_hood_raw %>% 
-      select(c("GSA_NAME", "geometry")) %>% 
-      rename("hood" = "GSA_NAME") %>% 
-      mutate(city = c("Halifax")) 
-  ),
-  
   tar_eval(
     tar_target(
-      names,
-      hood_cleaned(tars, substr(names, 1, 3))),
-    values = list(names = lapply(c('van_hood', 'cal_hood', 'win_hood', 'tor_hood', 'ott_hood', 'mon_hood', 'hal_hood'), as.symbol),
-                  tars = list(van_hood_i, cal_hood_i, win_hood_i, tor_hood_i, ott_hood_i, mon_hood_i, hal_hood_i))
-  ),
-  
-  tar_target(
-    can_hood,
-    rbind(cal_hood, hal_hood, mon_hood, ott_hood, tor_hood, van_hood, win_hood)
-  ),
-  
-  tar_target(
-    can_hood_ee,
-    can_hood %>%
-      sf_as_ee(x = .,
-             overwrite = TRUE,
-             assetId = sprintf("%s/%s", ee_get_assethome(), 'mun_road'),
-             bucket = 'rgee_dev',
-             monitoring = FALSE,
-             via = 'gcs_to_asset')
+      lapply(paste0('clean_', as.character(values_trib$file_name)), as.symbol),
+      cleaned_neighbourhoods()
+    ),
+    values = filter(values_trib, grepl('hood_raw', file_name))
   )
+  
+#  
+#  tar_target(
+#    can_hood,
+#    rbind(cal_hood, hal_hood, mon_hood, ott_hood, tor_hood, van_hood, win_hood)
+#  ),
+#  
+#  tar_target(
+#    can_hood_ee,
+#    can_hood %>%
+#      sf_as_ee(x = .,
+#             overwrite = TRUE,
+#             assetId = sprintf("%s/%s", ee_get_assethome(), 'mun_road'),
+#             bucket = 'rgee_dev',
+#             monitoring = FALSE,
+#             via = 'gcs_to_asset')
+#  )
 
 )
