2f0d580b933c7a8a2dbe729916c0f8827b9323df
icrichmond <icrichmond@mun.ca>
2022-12-07 11:18:21 -0500

    neighbourhoods to ee asset

--- scripts/data-cleanup/neighbourhood-data-cleanup.R
+++ scripts/data-cleanup/neighbourhood-data-cleanup.R
@@ -69,6 +69,17 @@ targets_neighbourhood_cleanup <- c(
   tar_target(
     can_hood,
     rbind(cal_hood, hal_hood, mon_hood, ott_hood, tor_hood, van_hood, win_hood)
+  ),
+  
+  tar_target(
+    can_hood_ee,
+    can_hood %>% 
+      sf_as_ee(x = .,
+             overwrite = TRUE,
+             assetId = sprintf("%s/%s", ee_get_assethome(), 'mun_road'),
+             bucket = 'rgee_dev',
+             monitoring = FALSE,
+             via = 'gcs_to_asset')
   )
 
 )
