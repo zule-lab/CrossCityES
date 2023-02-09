929a8a6fbc1d3ed934b08793c9fdadf3c6b2e545
icrichmond <icrichmond@mun.ca>
2023-02-09 13:32:51 -0500

    building_sf plus rbind

--- scripts/prepare-data/prepare-buildings.R
+++ scripts/prepare-data/prepare-buildings.R
@@ -3,9 +3,14 @@ targets_prepare_buildings <- c(
   tar_eval(
     tar_target(
       file_name_sym,
-      download_file(dl_link, dl_path, file_ext)
+      building_sf(dl_link, dl_path, file_ext)
     ),
     values = values_buildings
+  ),
+  
+  tar_target(
+    can_build,
+    rbind(cal_build, hal_build, mon_build, ott_build, tor_build, van_build, win_build)
   )
   
 )
