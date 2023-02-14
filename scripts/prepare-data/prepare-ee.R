72014343693a6262fb76217570e7123d60bb1457
icrichmond <icrichmond@mun.ca>
2023-02-14 15:33:43 -0500

    combine pollution data

--- scripts/prepare-data/prepare-ee.R
+++ scripts/prepare-data/prepare-ee.R
@@ -7,6 +7,18 @@ targets_prepare_ee <- c(
       clean_ee(dl_link, dl_path, file_ext)
     ),
     values = values_ee
+  ),
+  
+  # combine pollution for city level
+  tar_target(
+    cities_pollution,
+    cbind(cities_CO, cities_NO2[,-'city'], cities_O3[,-'city'], cities_SO2[,-'city'], cities_UV[,-'city'])
+  ),
+  
+  # combine pollution for neighbourhood level
+  tar_target(
+    neighbourhoods_pollution,
+    cbind(neighbourhoods_CO, neighbourhoods_NO2[,-c('city', 'hood', 'hood_area', 'hood_id')], neighbourhoods_O3[,-c('city', 'hood', 'hood_area', 'hood_id')], neighbourhoods_SO2[,-c('city', 'hood', 'hood_area', 'hood_id')], neighbourhoods_UV[,-c('city', 'hood', 'hood_area', 'hood_id')])
   )
   
   
