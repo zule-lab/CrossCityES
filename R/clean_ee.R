2ea54f28873ede3109d78dfecf1e55d8aff370bf
icrichmond <icrichmond@mun.ca>
2023-02-14 15:21:16 -0500

    clean ee downloads

--- R/clean_ee.R
+++ R/clean_ee.R
@@ -1,21 +1,74 @@
 clean_ee <- function(dl_link, dl_path, file_ext){
   
-  drive_download(file = dl_link, path = dl_path, type = file_ext)
+  drive_download(file = dl_link, path = dl_path, type = file_ext, overwrite = T)
   
+  # read downloaded file
   file <- read.csv(dl_path)
   
-  #ee <- clean_sat(file, dl_path)
+  # extract the folder path
+  folder <- dirname(dl_path)
   
-  return(file)
+  ee <- clean_sat(folder, file, dl_path)
+  
+  return(ee)
   
 }
 
-clean_sat <- function(file){
+clean_sat <- function(folder, file){
+  
+  if (folder == 'large/ndvi_ndbi'){
+    
+    label <- ""
+    
+    # remove system:index and geo columns because they are not in useable format
+    df <- file %>% select(-c('system.index', '.geo')) %>% 
+      rename_with(., ~ paste0(.x, "_", label), .cols = all_of(c("mean", "median", "max", "min", "count", "stdDev")))
+    
+    return(df)
+    
+    
+  }
+  
+  else if (folder == 'large/temperature'){
+    
+    label <- "temp"
+    
+    df <- file %>% select(-c('system.index', '.geo')) %>% 
+      rename_with(., ~ paste0(.x, "_", label), .cols = all_of(c("mean", "median", "max", "min", "count", "stdDev")))
+    
+    return(df)
+  
+    
+  }
+  
+  else if (folder == 'large/dem'){
+    
+    label <- "bldhgt"
+    
+    df <- file %>% select(-c('system.index', '.geo')) %>% 
+      rename_with(., ~ paste0(.x, "_", label), .cols = all_of(c("mean", "median", "max", "min", "count", "stdDev")))
+    
+    return(df)
+
+    
+  }
+  
+  else if (folder == 'large/pollution'){
+    
+    label <- basename(sans_ext(dl_path))
+    
+    df <- file %>% select(-c('system.index', '.geo')) %>% 
+      rename_with(., ~ paste0(.x, "_", label), .cols = all_of(c("mean", "median", "max", "min", "count", "stdDev")))
+    
+    return(df)
+    
+    
+  }
   
   
-  
-  
-  
+  else {
+    print("error: none of the folder paths matched")
+  }
   
   
 }
--- R/clean_pollution.R
+++
@@ -1,9 +0,0 @@
-clean_pollution <- function(df, label){
-
-  # remove system:index and geo columns because they are not in useable format
-  df <- df %>% select(-c('system:index', '.geo')) %>% 
-    rename_with(., ~ paste0(.x, "_", label), .cols = all_of(c("mean", "median", "max", "min", "count", "stdDev")))
-  
-  return(df)
-  
-}
