f8d0b83c0cfb3e2e1635bea30a01ecfbfa487e7e
icrichmond <icrichmond@mun.ca>
2023-02-17 10:35:38 -0500

    summarize tree-density into one function

--- R/tree_density.R
+++ R/tree_density.R
@@ -1,13 +1,13 @@
-targets_treedensity_city_neighbourhood_road <- c(
+tree_density <- function(can_trees, scale, treesize, builddens){
   
-  tar_target(
-    treedensity_city, 
-    all_tree %>% 
+  if (scale == 'city'){
+    
+    treedensity_city <- can_trees %>% 
       group_by(city) %>%
       st_set_geometry(NULL) %>%
       summarize(nTrees = n()) %>%
-      inner_join(., as.data.frame(treesize_city), by = "city") %>% 
-      inner_join(., as.data.frame(can_build_city_dens), by = "city") %>%
+      inner_join(., as.data.frame(treeesize), by = "city") %>% 
+      inner_join(., as.data.frame(builddens), by = "city") %>%
       summarize(city = city,
                 nTrees = nTrees,
                 mean_ba = mean_ba, # sq ft
@@ -15,16 +15,19 @@ targets_treedensity_city_neighbourhood_road <- c(
                 stemdens = nTrees/cityarea, # trees/km2
                 stemdens_acre = stemdens/247.105, # conversion from num trees/km2 to num trees/acre
                 basaldens = mean_ba*stemdens_acre) # sq ft/acre
-  ),
+    
+    return(treedensity_city)
+    
+  }
   
-  tar_target(
-    treedensity_hood, 
-    all_tree %>% 
+  else if (scale == 'neighbourhood'){
+    
+    treedensity_neighbourhood <- can_trees %>% 
       group_by(city, hood_id) %>%
       st_set_geometry(NULL) %>%
       summarize(nTrees = n()) %>%
-      inner_join(., as.data.frame(treesize_hood), by = "hood_id") %>% 
-      inner_join(., as.data.frame(can_build_hood_dens), by = "hood_id") %>%
+      inner_join(., as.data.frame(treesize), by = "hood_id") %>% 
+      inner_join(., as.data.frame(builddens), by = "hood_id") %>%
       summarize(city = city,
                 hood_id = hood_id,
                 nTrees = nTrees,
@@ -33,16 +36,19 @@ targets_treedensity_city_neighbourhood_road <- c(
                 stemdens = nTrees/hoodarea,
                 stemdens_acre = stemdens/247.105,
                 basaldens = mean_ba*stemdens_acre)
-  ),
+    
+    return(treedensity_neighbourhood)
+    
+  }
   
-  tar_target(
-    treedensity_road, # SOMETHING WEIRD IS HAPPENING
-    all_tree %>% 
+  else if (scale == 'road'){
+    
+    treedensity_road <- can_trees %>% 
       group_by(city, hood_id, streetid) %>%
       summarize(nTrees = n()) %>%
       mutate(streetid = as.character(streetid)) %>%
-      inner_join(., as.data.frame(treesize_road), by = "streetid") %>% 
-      inner_join(., as.data.frame(can_build_road_dens), by = "streetid") %>%
+      inner_join(., as.data.frame(treesize), by = "streetid") %>% 
+      inner_join(., as.data.frame(builddens), by = "streetid") %>%
       summarize(city = city,
                 hood_id = hood_id.x,
                 streetid = streetid,
@@ -52,7 +58,11 @@ targets_treedensity_city_neighbourhood_road <- c(
                 stemdens = nTrees/roadlength,
                 stemdens_acre = stemdens/247.105,
                 basaldens = mean_ba*stemdens_acre)
-  )
+    
+  }
   
+  else {
+    print("error: none of the scales matched")
+  }
   
-)
+}
---
+++ scripts/natural/tree-density.R
@@ -0,0 +1,19 @@
+targets_tree_density <- c(
+  
+  tar_target(
+    cities_treedensity, 
+    tree_density(can_trees, 'city', cities_treesize, build_dens_city)
+  ),
+  
+  tar_target(
+    treedensity_hood, 
+    tree_density(can_trees, 'neighbourhood', neighbourhood_treesize, build_dens_neighbourhood)
+  ),
+  
+  tar_target(
+    treedensity_road,
+    tree_density(can_trees, 'road', road_treesize, build_dens_road)
+  )
+  
+  
+)
