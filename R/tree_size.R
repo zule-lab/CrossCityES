c00a3546a1ab2a2ccad4690df9882a1b95a71f52
icrichmond <icrichmond@mun.ca>
2023-02-17 10:13:00 -0500

    tree size to one function

---
+++ R/tree_size.R
@@ -0,0 +1,48 @@
+tree_size <- function(can_trees, scale){
+  
+  
+  all_tree_in <- can_trees %>% 
+    drop_na(dbh) %>%
+    mutate(dbh_in = dbh*2.54,
+           basal_area = ((dbh_in)^2 * 0.005454))
+  
+  if (scale == 'city'){
+    
+    grouped <- all_tree_in %>%
+      group_by(city)
+    
+  }
+  
+  else if (scale == 'neighbourhood'){
+    
+    grouped <- all_tree_in %>%
+      group_by(city, hood_id)
+    
+  }
+  
+  else if (scale == 'road'){
+   
+     grouped <- all_tree_in %>%
+      group_by(city, hood_id, streetid)
+    
+  }
+  
+  else {
+    print("error: none of the scales matched")
+  }
+
+  
+  size <- grouped %>%
+    summarize(mean_ba = mean(basal_area),
+              sd_ba = sd(basal_area),
+              mean_dbh = mean(dbh_in),
+              sd_dbh = mean(dbh_in)) %>%
+    mutate(mean_ba = set_units(mean_ba, "ft2"),
+           sd_ba = set_units(sd_ba, "ft2"),
+           mean_dbh = set_units(mean_dbh, "in"),
+           sd_dbh = set_units(sd_dbh, "in"))
+
+  return(size)
+  
+  
+}
--- scripts/natural/tree-size.R
+++ scripts/natural/tree-size.R
@@ -1,58 +1,18 @@
 targets_tree_size <- c(
   
   tar_target(
-    all_tree_in, 
-    all_tree %>% 
-      drop_na(dbh) %>%
-      mutate(dbh_in = dbh*2.54,
-             basal_area = ((dbh_in)^2 * 0.005454))
+    cities_treesize,
+    tree_size(can_trees, 'city')
   ),
   
   tar_target(
-    treesize_city, 
-    all_tree_in %>%
-      group_by(city) %>% 
-      summarize(mean_ba = mean(basal_area),
-                sd_ba = sd(basal_area),
-                mean_dbh = mean(dbh_in),
-                sd_dbh = mean(dbh_in)) %>%
-      mutate(mean_ba = set_units(mean_ba, "ft2"),
-             sd_ba = set_units(sd_ba, "ft2"),
-             mean_dbh = set_units(mean_dbh, "in"),
-             sd_dbh = set_units(sd_dbh, "in"))
-    
+    neighbourhood_treesize,
+    tree_size(can_trees, 'neighbourhood')
   ),
   
   tar_target(
-    treesize_hood, 
-    all_tree_in %>%
-      group_by(city, hood_id) %>% 
-      summarize(mean_ba = mean(basal_area), # NOTE: in square feet
-                sd_ba = sd(basal_area),
-                mean_dbh = mean(dbh_in),
-                sd_dbh = sd(dbh_in)) %>%
-      mutate(mean_ba = set_units(mean_ba, "ft2"),
-             sd_ba = set_units(sd_ba, "ft2"),
-             mean_dbh = set_units(mean_dbh, "in"),
-             sd_dbh = set_units(sd_dbh, "in"))
-    
-  ),
-  
-  
-  tar_target(
-    treesize_road, 
-    all_tree_in %>%
-      group_by(city, hood_id, streetid) %>% 
-      summarize(mean_ba = mean(basal_area), # NOTE: in square feet
-                sd_ba = sd(basal_area),
-                mean_dbh = mean(dbh_in),
-                sd_dbh = sd(dbh_in)) %>%
-      mutate(streetid = as.character(streetid),
-             mean_ba = set_units(mean_ba, "ft2"),
-             sd_ba = set_units(sd_ba, "ft2"),
-             mean_dbh = set_units(mean_dbh, "in"),
-             sd_dbh = set_units(sd_dbh, "in"))
-    
+    road_treesize,
+    tree_size(can_trees, 'road')
   )
   
   
