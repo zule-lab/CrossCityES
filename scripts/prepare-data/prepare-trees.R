8f2c7b1c4f2a42325ed258bc896aab8d494f69c7
icrichmond <icrichmond@mun.ca>
2023-02-17 09:10:21 -0500

    deleted species # viz and species list ,moved full tree clean to prepare-trees

--- scripts/prepare-data/prepare-trees.R
+++ scripts/prepare-data/prepare-trees.R
@@ -36,5 +36,32 @@ targets_prepare_trees <- c(
     rbind(van_tree_clean, cal_tree_clean, win_tree_clean,
           tor_tree_clean, ott_tree_clean, mon_tree_clean, 
           hal_tree_clean)
-  )
+  ),
+  
+  tar_target(
+    all_tree_e,
+    all_tree_raw %>%
+      drop_na(city) %>%
+      mutate(species = as.factor(species),
+             genus = as.factor(genus),
+             city = as.factor(city),
+             fullname = as.factor(paste0(genus, " ", species))) %>%
+      filter(dbh < 500) %>%
+      group_by(city) %>%
+      mutate(nspecies = n_distinct(fullname))
+  ), 
+  
+  tar_target(
+    all_tree,
+    assign_sp_all(all_tree_e)
+  ),
+  
+  tar_target(
+    abundance,
+    all_tree %>%
+      group_by(city, fullname) %>%
+      summarise(n = n()) %>%
+      mutate(freq = (n / sum(n))*100) %>%
+      arrange(desc(freq), .by_group = T)
+  ),
 )
