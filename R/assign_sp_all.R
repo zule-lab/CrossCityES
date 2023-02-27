7da3be3a8dd1ad395d4f47dabfe8ea293871319f
icrichmond <icrichmond@mun.ca>
2023-02-27 15:35:55 -0500

    add new typos to species name

--- R/assign_sp_all.R
+++ R/assign_sp_all.R
@@ -29,16 +29,38 @@ assign_sp_all <- function(van_tree_clean, cal_tree_clean, win_tree_clean, tor_tr
   all$fullname[all$fullname %in% c("Aesculus carnea", "Aesculus carnea   x")]<- "Aesculus x carnea"
   # Amelanchier x grandiflora 
   all$fullname[all$fullname %in% c("Amelanchier grandiflora x")]<- "Amelanchier x grandiflora"
+  # Betula platyphylla
+  all$fullname[all$fullname %in% c("Betula platyphylla jefpark")]<- "Betula platyphylla"
+  # Caragana arborescens 
+  all$fullname[all$fullname %in% c("Caragana arboresense", "Caragana arobrescens")]<- "Caragana arborescens"
   # Cercidiphyllum japonicum
   all$fullname[all$fullname %in% c("Cercidiphyllym japonicum", "Cercidiphyllum japonic")]<- "Cercidiphyllum japonicum"
   # Cladrastis kentukea
   all$fullname[all$fullname %in% c("Cladastris kentukea")]<- "Cladrastis kentukea"
+  # Chamaecyparis nootkatensis
+  all$fullname[all$fullname %in% c("Chamaecyparis nootkensis")]<- "Chamaecyparis nootkatensis"
+  # Cladastris lutea
+  all$fullname[all$fullname %in% c("Cladrastis lutea")]<- "Cladastris lutea"
   # Crataegus crus-galli 
   all$fullname[all$fullname %in% c("Crataegus crus", "Crataegus crusgalli")]<- "Crataegus crus-galli"
+  # Crataegus laevigata
+  all$fullname[all$fullname %in% c("Cartaegus laevigata")]<- "Crataegus laevigata"
   # Crataegus × mordenensis
   all$fullname[all$fullname %in% c("Crataegus mordenensis", "Crataegus mordensis", "Crataegus x mordensis")]<- "Crataegus x mordenensis"
+  # Crataegus x oxyacantha
+  all$fullname[all$fullname %in% c("Crataegus oxyacantha")]<- "Crataegus x oxyacantha"
   # Euonymus alatus
   all$fullname[all$fullname %in% c("Euonymus altus")]<- "Euonymus alatus"
+  # Fraxinus pennsylvanica
+  all$fullname[all$fullname %in% c("Fraxinus pennsylvancia")]<- "Fraxinus pennsylvanica"
+  # Gleditsia triacanthos
+  all$fullname[all$fullname %in% c("Gleditisia triacanthos", "Gleditsia t.")]<- "Gleditsia triacanthos"
+  # Hydrangea paniculata
+  all$fullname[all$fullname %in% c("Hydrangea paniculata grandiflora")]<- "Hydrangea paniculata"
+  # Juglans ailantifolia
+  all$fullname[all$fullname %in% c("Juglans ailanthifolia")]<- "Juglans ailantifolia"
+  # Juglans cinerea
+  all$fullname[all$fullname %in% c("Juglans cinereax")]<- "Juglans cinerea"
   # Laburnum x watereri
   all$fullname[all$fullname %in% c("Laburnum watereri  x")]<- "Laburnum x watereri"
   # Liquidambar styraciflua
@@ -51,10 +73,14 @@ assign_sp_all <- function(van_tree_clean, cal_tree_clean, win_tree_clean, tor_tr
   all$fullname[all$fullname %in% c("Ostryia virginiana")]<- "Ostrya virginiana"
   # Phellodendron amurense
   all$fullname[all$fullname %in% c("Phellodendon amurense", "Phellodendron amurensis")]<- "Phellodendron amurense"
+  # Philadelphus lewisii
+  all$fullname[all$fullname %in% c("Philadelphus lewissii")]<- "Philadelphus lewisii"
   # Picea engelmannii
   all$fullname[all$fullname %in% c("Picea englemannii")]<- "Picea engelmannii"
   # Picea mariana 
   all$fullname[all$fullname %in% c("Picea marina")]<- "Picea mariana"
+  # Picea omorika
+  all$fullname[all$fullname %in% c("Picea omorica")]<- "Picea omorika"
   # Picea pungens
   all$fullname[all$fullname %in% c("Picea punges")]<- "Picea pungens"
   # Pinus sylvestris
@@ -69,6 +95,10 @@ assign_sp_all <- function(van_tree_clean, cal_tree_clean, win_tree_clean, tor_tr
   all$fullname[all$fullname %in% c("Prunus armeniaca")]<- "Prunus americana"
   # Prunus maackii
   all$fullname[all$fullname %in% c("Prunus maakii")]<- "Prunus maackii"
+  # Prunus pensylvanica
+  all$fullname[all$fullname %in% c("Prunus pennsylvanica")]<- "Prunus pensylvanica"
+  # Prunus x nigrella
+  all$fullname[all$fullname %in% c("Prunus nigrella")]<- "Prunus x nigrella"
   # Pseudotsuga menziesii
   all$fullname[all$fullname %in% c("Pseudostuga menziesii")]<- "Pseudotsuga menziesii"
   # Pterocarya stenoptera
@@ -86,7 +116,7 @@ assign_sp_all <- function(van_tree_clean, cal_tree_clean, win_tree_clean, tor_tr
   # Ulmus x hollandica 
   all$fullname[all$fullname %in% c("Ulmus hollandica   x")]<- "Ulmus x hollandica"
   # Unknown sp.
-  all$fullname[all$fullname %in% c("NA sp.", "See notes", "Various sp.")] <- "Unknown sp."
+  all$fullname[all$fullname %in% c("NA sp.", "See notes", "Various sp.", "Not suitable", "Populus deltoides / balsamifera (?)")] <- "Unknown sp."
   
   return(all)
   
