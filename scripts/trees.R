c9ef12b9cf8df938859db170733f8930c4a39e80
i_richmo <i_richmo@BIOL-E6F40C.concordia.ca>
2025-04-11 15:18:01 -0400

    add func groups

--- scripts/trees.R
+++ scripts/trees.R
@@ -22,24 +22,25 @@ targets_trees <- c(
 
 # richness ----------------------------------------------------------------
 
-  tar_target(
-    cities_treerichness,
-    tree_richness(can_trees, 'city')
-  ),
-  
-  tar_target(
-    neighbourhood_treerichness,
-    tree_richness(can_trees, 'neighbourhood')
-  ),
-  
-  tar_target(
-    road_treerichness,
-    tree_richness(can_trees, 'road', road_bound_trees)
-  ),
 
   tar_target(
     func_groups, 
     create_func_groups(can_trees, TTTF_1.3, seed_mass, TTTF_newlit, ZULE_traits)
+  ),
+
+  tar_target(
+    cities_treerichness,
+    tree_richness(can_trees, 'city', func_groups)
+  ),
+  
+  tar_target(
+    neighbourhood_treerichness,
+    tree_richness(can_trees, 'neighbourhood', func_groups)
+  ),
+  
+  tar_target(
+    road_treerichness,
+    tree_richness(can_trees, 'road', func_groups, road_bound_trees)
   ),
 
 
