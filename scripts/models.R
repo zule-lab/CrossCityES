158826f0a7d35494a7a9eec35d662be1cc817de8
icrichmond <52459005+icrichmond@users.noreply.github.com>
2025-06-04 14:05:19 -0400

    add air polllution

--- scripts/models.R
+++ scripts/models.R
@@ -34,13 +34,39 @@ targets_models <- c(
   
   tar_target(
     neighbourhoods_temp_tune,
-    rf_tune(neighbourhoods_lst_full)
+    rf_tune(neighbourhoods_lst_full %>% rename(value = mean_temp))
+  ),
+  
+  tar_target(
+    neighbourhoods_uv_tune, 
+    rf_tune(neighbourhoods_pollution_full %>% filter(variable == 'mean_cities_UV') %>% select(-variable))
+  ),
+  
+  tar_target(
+    neighbourhoods_co_tune,
+    rf_tune(neighbourhoods_pollution_full %>% filter(variable == 'mean_cities_CO') %>% select(-variable))
+  ),
+  
+  tar_target(
+    neighbourhoods_no2_tune,
+    rf_tune(neighbourhoods_pollution_full %>% filter(variable == 'mean_cities_NO2') %>% select(-variable))
+  ),
+  
+  tar_target(
+    neighbourhoods_o3_tune,
+    rf_tune(neighbourhoods_pollution_full %>% filter(variable == 'mean_cities_O3') %>% select(-variable))
+  ),
+  
+  tar_target(
+    neighbourhoods_so2_tune,
+    rf_tune(neighbourhoods_pollution_full %>% filter(variable == 'mean_cities_SO2') %>% select(-variable))
   ),
   
   tar_target(
     streets_temp_tune, 
     # model a subset because dataset is too large (1000 street segments / city)
-    rf_tune(roads_lst_full %>% group_by(city) %>% sample_n(1000))
+    rf_tune(roads_lst_full %>% rename(value = mean_temp) %>% 
+              group_by(city) %>% sample_n(1000))
   )
   
   
