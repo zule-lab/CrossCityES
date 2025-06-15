9dc2476e99ff4910ac6866e06c850fba5d0736ea
icrichmond <52459005+icrichmond@users.noreply.github.com>
2025-06-14 22:37:25 -0400

    selection typo

--- R/combine_neighbourhoods_pollution.R
+++ R/combine_neighbourhoods_pollution.R
@@ -65,13 +65,13 @@ combine_neighbourhoods_pollution <- function(neighbourhoods_pollution, neighbour
     mutate(city_hood = paste0(city, "_", hood)) %>%
     select(-c(time, hood,
               coverage, nTrees, stemdens_acre,
-              total_ba, hood_area.y, hood_id.x.x, hood_area.x.x,
+              total_ba, hood_area.y, hood_id.x.x,
               centroids, build_area, prop_strts, road_length,
               neighbourhood_area, area, DSAcount, lowinc,
-              hood_id.y.y, hood_id, date_ndvi, time_ndvi, NDBI_count_,
+              hood_id.y.y, date_ndvi, time_ndvi, NDBI_count_,
               NDBI_median_, NDBI_max_, NDBI_min_, NDBI_stdDev_,
               NDVI_count_, NDVI_median_, NDVI_max_, NDVI_min_, 
-              NDVI_stdDev_, hood_area.y.y, id, diff)) %>% 
+              NDVI_stdDev_, diff)) %>% 
     mutate_if(is.character, factor) %>%
     mutate(doy = yday(date)) %>% 
     select(-date)
