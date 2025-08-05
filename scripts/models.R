f53600c6d650710e7058079801ae2b42dd61a398
icrichmond <52459005+icrichmond@users.noreply.github.com>
2025-08-05 17:12:54 -0400

    subset roads

--- scripts/models.R
+++ scripts/models.R
@@ -3,20 +3,23 @@ targets_models <- c(
   
   tar_target(
     model_data, 
-    list(cities_lst_full %>% rename(value = mean_temp),
+    list(cities_lst_full %>% dplyr::rename(value = mean_temp),
          cities_pollution_full %>% filter(variable == 'mean_UV_city') %>% select(-variable),
          cities_pollution_full %>% filter(variable == 'mean_CO_city') %>% select(-variable),
          cities_pollution_full %>% filter(variable == 'mean_NO2_city') %>% select(-variable),
          cities_pollution_full %>% filter(variable == 'mean_O3_city') %>% select(-variable),
          cities_pollution_full %>% filter(variable == 'mean_SO2_city') %>% select(-variable),
-         neighbourhoods_lst_full %>% rename(value = mean_temp),
+         neighbourhoods_lst_full %>% dplyr::rename(value = mean_temp),
          neighbourhoods_pollution_full %>% filter(variable == 'mean_UV_neighbourhood') %>% select(-variable),
          neighbourhoods_pollution_full %>% filter(variable == 'mean_CO_neighbourhood') %>% select(-variable),
          neighbourhoods_pollution_full %>% filter(variable == 'mean_NO2_neighbourhood') %>% select(-variable),
          neighbourhoods_pollution_full %>% filter(variable == 'mean_O3_neighbourhood') %>% select(-variable),
          neighbourhoods_pollution_full %>% filter(variable == 'mean_SO2_neighbourhood') %>% select(-variable),
-         roads_lst_full %>% rename(value = mean_temp) %>% 
-           group_by(city) %>% sample_n(1000)) %>% 
+         roads_lst_full %>% 
+           dplyr::rename(value = mean_temp) %>% 
+           group_by(city) %>% 
+           sample_n(1000) %>%
+           select(-c(streetdir, streettype)) ) %>% 
       setNames(., c('cities_temp', 'cities_UV', 'cities_CO', 'cities_NO2', 'cities_O3', 'cities_SO2',
                'neighbourhoods_temp', 'neighbourhoods_UV', 'neighbourhoods_CO', 'neighbourhoods_NO2', 'neighbourhoods_O3', 'neighbourhoods_SO2',
                'streets_temp'))
