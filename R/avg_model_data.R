45ce5ee6fa159e0f564b807b011cfc7214252eb4
icrichmond <52459005+icrichmond@users.noreply.github.com>
2025-12-08 17:01:07 -0500

    remove city dataframes, too small (n = 7)

--- R/avg_model_data.R
+++ R/avg_model_data.R
@@ -1,44 +1,5 @@
 avg_model_data <- function(model_data, roads_lst_full){
   
-  cities <- model_data %>% 
-    keep(str_detect(names(model_data), 'cities')) %>% 
-    map(\(x) x %>% group_by(city) %>% summarize(value = mean(value),
-                                                NDBI_mean_ = mean(NDBI_mean_),
-                                                NDVI_mean_ = mean(NDVI_mean_),
-                                                ba_per_m2 = first(ba_per_m2), 
-                                                cityarea = first(cityarea), 
-                                                stemdens = first(stemdens), 
-                                                SpeciesRichness = first(SpeciesRichness), 
-                                                Shannon = first(Shannon), 
-                                                FG_richness = first(FG_richness),
-                                                FG_shannon = first(FG_shannon), 
-                                                mean_dbh = first(mean_dbh), 
-                                                sd_dbh = first(sd_dbh), 
-                                                centroid_den = first(centroid_den), 
-                                                area_den = first(area_den),
-                                                prop_highway = first(prop_highway), 
-                                                #prop_strts = first(prop_strts), 
-                                                road_dens = first(road_dens),
-                                                popdens = first(popdens), 
-                                                sidehop = first(sidehop), 
-                                                aptfivep = first(aptfivp), 
-                                                semhoup = first(semhoup), 
-                                                rowhoup = first(rowhoup), 
-                                                aptdupp = first(aptdupp), 
-                                                aptbuip = first(aptbuip), 
-                                                mvdwelp = first(mvdwelp), 
-                                                medinc = first(medinc), 
-                                                recimmp = first(recimmp), 
-                                                indigp = first(indigp), 
-                                                visminp = first(visminp), 
-                                                edubacp = first(edubacp), 
-                                                totpop = first(totpop), 
-                                                lowincp = first(lowincp), 
-                                                mean_bldhgt = first(mean_bldhgt), 
-                                                stdDev_bldhgt = first(stdDev_bldhgt), 
-                                                lon = first(lon),
-                                                lat = first(lat)))
-  
   neighbourhoods <- model_data %>% 
     keep(str_detect(names(model_data), 'neighbourhoods')) %>% 
     map(\(x) x %>% group_by(city_hood) %>% summarize(value = mean(value),
@@ -88,7 +49,7 @@ avg_model_data <- function(model_data, roads_lst_full){
               NDBI_mean_ = mean(NDBI_mean_),
               NDVI_mean_ = mean(NDVI_mean_),
               city = first(city),
-              #road_class = first(road_class),
+              road_class = first(road_class),
               ba_per_m2 = first(ba_per_m2),
               stemdens = first(stemdens), 
               SpeciesRichness = first(SpeciesRichness), 
@@ -119,9 +80,8 @@ avg_model_data <- function(model_data, roads_lst_full){
               lon = first(lon),
               lat = first(lat)))
   
-  list <-c(cities, neighbourhoods, streets) %>% 
-    setNames(., c('cities_temp', 'cities_UV', 'cities_CO', 'cities_NO2', 'cities_O3', 'cities_SO2',
-                  'neighbourhoods_temp', 'neighbourhoods_UV', 'neighbourhoods_CO', 'neighbourhoods_NO2', 'neighbourhoods_O3', 'neighbourhoods_SO2',
+  list <-c(neighbourhoods, streets) %>% 
+    setNames(., c('neighbourhoods_temp', 'neighbourhoods_UV', 'neighbourhoods_CO', 'neighbourhoods_NO2', 'neighbourhoods_O3', 'neighbourhoods_SO2',
                   'streets_temp'))
   
   return(list)
